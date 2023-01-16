076ede397a61fc4753a66f8e45ce04a9eb3f4a94
Marc-Eid <marc.mn.eid@gmail.com>
2023-01-16 12:54:30 -0500

    #16 - added some user related fields

--- backend/src/models/user.entity.ts
+++ backend/src/models/user.entity.ts
@@ -1,59 +1,98 @@
 import { ApiProperty } from "@nestjs/swagger";
 import {
-	BaseEntity,
-	Column,
-	CreateDateColumn,
-	Entity,
-	Index,
-	OneToMany,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  RelationCount,
+  UpdateDateColumn,
 } from "typeorm";
-
+import { Education } from "./education.entity";
+import { Skill } from "./skill.entity";
+import { Volunteering } from "./volunteering.entity";
+import { Work } from "./work.entity";
 
 @Entity("users")
 export class User extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	@ApiProperty()
-	id: number;
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
 
-	@Column()
-	@Index({ unique: true })
-	@ApiProperty()
-	email: string;
+  @Column()
+  @Index()
+  @ApiProperty()
+  firstName: string;
 
-	@Column({ select: false })
-	password: string;
+  @Column({default: null})
+  @Index()
+  @ApiProperty()
+  lastName: string;
 
-	@Column()
-	@Index()
-	@ApiProperty()
-	firstName: string;
+  @Column()
+  @Index({ unique: true })
+  @ApiProperty()
+  email: string;
 
-	@Column()
-	@Index()
-	@ApiProperty()
-	lastName: string;
+  @Column({ select: false })
+  password: string;
 
-	@Column()
-	@ApiProperty()
-	gender: "male" | "female";
+  @Column({default: null})
+  @ApiProperty()
+  mobileNo: string;
 
-	@CreateDateColumn()
-	@ApiProperty()
-	created_at: Date;
+  @Column()
+  @ApiProperty()
+  gender: "male" | "female";
 
-	@UpdateDateColumn()
-	@ApiProperty()
-	updated_at: Date;
+  @Column({default: null})
+  @ApiProperty()
+  biography: string;
 
-	// RELATIONS
-	// @OneToMany(() => Education, (e) => e.user)
-	// @ApiProperty({ type: [Education] })
-	// educations: Education[];
+  @CreateDateColumn()
+  @ApiProperty()
+  created_at: Date;
 
-	// SPECIAL GETTERS
-	public get fullName() {
-		return this.firstName + " " + this.lastName;
-	}
+  @UpdateDateColumn()
+  @ApiProperty()
+  updated_at: Date;
+
+  // RELATIONS
+
+  //education
+  @OneToMany(() => Education, (e) => e.user)
+  @ApiProperty({ type: [Education] })
+  educations: Education[];
+
+  //work experience
+  @OneToMany(() => Work, (w) => w.user)
+  @ApiProperty({ type: [Work] })
+  workExperience: Work[];
+
+  //volunteering experience
+  @OneToMany(() => Volunteering, (v) => v.user)
+  @ApiProperty({ type: [Volunteering] })
+  volunteeringExperience: Volunteering[];
+
+  //connections
+  @ManyToMany(() => User, (user) => user.connections)
+  @ApiProperty({ type: [User] })
+  connections: User[];
+
+
+
+  //skills
+  @ManyToMany((type) => Skill, (skill) => skill.user)
+  @JoinTable()
+  @ApiProperty({ type: [Skill] }) 
+  skills: Skill[];
+
+  // SPECIAL GETTERS
+  public get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
 }
