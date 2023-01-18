7781635c8dc562d1c8e44800c567bd9b419c5317
Usama Saleem <usama.saleem@hotmail.com>
2023-01-18 16:09:40 -0500

    nav bar implemented

---
+++ skillswipe/src/components/NavBar.tsx
@@ -0,0 +1,155 @@
+import React from "react";
+import Link from "next/link";
+import { useState } from 'react'
+import {
+  useColorMode,
+  Switch,
+  Flex,
+  Button,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+import NextLink from 'next/link'
+
+export default function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  const isDark = colorMode === 'dark'
+  const [display, changeDisplay] = useState('none')
+  const toggleTheme = useColorModeValue("🌙", "💡")
+
+
+      return (
+    <Flex>
+      <Flex
+        position="fixed"
+        top="1rem"
+        right="1rem"
+        align="center"
+      >
+        {/* Desktop */}
+        
+        <Flex
+          display={['none', 'none', 'flex','flex']}
+        >
+          <NextLink href="/home" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Home"
+              my={5}
+              w="100%"
+            >
+              Home
+                    </Button>
+          </NextLink>
+
+          <NextLink href="/swipe" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Swipe"
+              my={5}
+              w="100%"
+            >
+              Swipe
+                    </Button>
+          </NextLink>
+
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Sign In/Logout"
+              my={5}
+              w="100%"
+            >
+              Sign In/Logout
+                    </Button>
+          </NextLink>
+        </Flex>
+
+        {/* Mobile */}
+        <IconButton
+          aria-label="Open Menu"
+          size="lg"
+          mr={2}
+          icon={
+            <HamburgerIcon />
+          }
+          onClick={() => changeDisplay('flex')}
+          display={['flex', 'flex', 'none', 'none']}
+        />
+        <Button onClick={toggleColorMode}>{toggleTheme}</Button>
+      </Flex>
+
+      {/* Mobile Content */}
+      <Flex
+        w='100vw'
+        display={display}
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Open Menu"
+            size="lg"
+            icon={
+              <CloseIcon />
+            }
+            onClick={() => changeDisplay('none')}
+          />
+        </Flex>
+
+        <Flex
+          flexDir="column"
+          align="center"
+        >
+          <NextLink href="/home" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Home"
+              my={5}
+              w="100%"
+            >
+              Home
+                    </Button>
+          </NextLink>
+
+          <NextLink href="/swipe" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Swipe"
+              my={5}
+              w="100%"
+            >
+              Swipe
+                    </Button>
+          </NextLink>
+
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Sign In"
+              my={5}
+              w="100%"
+            >
+              Sign In/Logout
+            </Button>
+          </NextLink>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
--- skillswipe/src/pages/home.tsx
+++ skillswipe/src/pages/home.tsx
@@ -1,5 +1,8 @@
 import { Flex, Heading, Input, Button, useColorMode, useColorModeValue} from '@chakra-ui/react'
 import Link from 'next/link';
+import Head from "next/head";
+import type { NextPage } from "next";
+import NavBar from '@/components/NavBar';
 
 const Home = () => {
   const { toggleColorMode } = useColorMode()
@@ -8,15 +11,10 @@ const Home = () => {
   const toggleTheme = useColorModeValue("🌙", "💡")
 
 return (
-<Flex height="100vh" alignItems="center" justifyContent="center">
-  <Flex direction="column" background={formBackground} p={12} rounded={25} >
-    <Heading mb={6}>Home</Heading>
-    <Button colorScheme="blue" mb={4}>
-      <Link href="/">Go back</Link>
-      </Button>
-    <Button onClick={toggleColorMode} variant="contained">{toggleTheme}</Button>
-  </Flex>
-</Flex>
+
+      <>
+      <NavBar></NavBar>
+    </>
 )
 
   
