4a2e1d8a3b791c97ccab85dfb9ca0ffc027aa363
Usama Saleem <usama.saleem@hotmail.com>
2023-01-18 17:57:15 -0500

    Create swipe.tsx

---
+++ skillswipe/src/pages/swipe.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
+
+const swipe = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Text>Swipe page</Text>
+    </>
+  );
+};
+
+export default swipe;
