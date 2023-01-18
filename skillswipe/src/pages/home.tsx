a053554e6eadaad53c75899e35c1fd7c9b3d379b
Usama Saleem <usama.saleem@hotmail.com>
2023-01-18 17:09:38 -0500

    Update home.tsx

--- skillswipe/src/pages/home.tsx
+++ skillswipe/src/pages/home.tsx
@@ -21,7 +21,11 @@ const Home = () => {
 
   const [posts, setPosts] = useState([
     { id: 1, title: "First Post", body: "This is a description" },
-    { id: 2, title: "Second Post", body: "This is a description" },
+    {
+      id: 2,
+      title: "Second Post",
+      body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    },
     { id: 3, title: "Third Post", body: "This is a description" },
   ]);
 
@@ -46,7 +50,7 @@ const Home = () => {
                   backgroundColor={postBackground}
                   padding="1rem"
                   marginBottom="1rem"
-                  rounded="2xl"
+                  rounded="20"
                   overflow="hidden"
                   width="100%"
                   minW="80vw"
