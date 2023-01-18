22cd5b04564fbff412757b6e848fc216dd3fc297
Usama Saleem <usama.saleem@hotmail.com>
2023-01-18 15:24:51 -0500

    navigation logic with chakra

--- skillswipe/src/pages/index.tsx
+++ skillswipe/src/pages/index.tsx
@@ -5,7 +5,8 @@ const IndexPage = () => {
   const { toggleColorMode } = useColorMode()
   const formBackground = useColorModeValue("gray.100", "gray.700")
   const placeholderBackground = useColorModeValue("gray.200", "gray.600")
-  
+    const toggleTheme = useColorModeValue("🌙", "💡")
+
 return (
 <Flex height="100vh" alignItems="center" justifyContent="center">
   <Flex direction="column" background={formBackground} p={12} rounded={25} >
@@ -16,7 +17,7 @@ return (
       <Link href="/home">Sign In</Link>
       </Button>
     <Button colorScheme="green" mb={6}>Register</Button>
-    <Button onClick={toggleColorMode}>Toggle Theme</Button>
+    <Button onClick={toggleColorMode}>{toggleTheme}</Button>
   </Flex>
 </Flex>
 )
