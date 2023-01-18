fb3a4a6ed4c1d1a2288def6e2ac583373625e37e
Usama Saleem <usama.saleem@hotmail.com>
2023-01-18 15:09:13 -0500

    Setup front end
    With chakra UI, login page

---
+++ skillswipe/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { Flex, Heading, Input, Button, useColorMode, useColorModeValue} from '@chakra-ui/react'
+import Link from 'next/link';
+
+const IndexPage = () => {
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue("gray.100", "gray.700")
+  const placeholderBackground = useColorModeValue("gray.200", "gray.600")
+  
+return (
+<Flex height="100vh" alignItems="center" justifyContent="center">
+  <Flex direction="column" background={formBackground} p={12} rounded={25} >
+    <Heading mb={6}>Skillswipe</Heading>
+    <Input placeholder="Email" variant="filled" mb={3} type="email" background={placeholderBackground} />
+    <Input placeholder="*******" variant="filled" mb={6} type="password" background={placeholderBackground}/>
+    <Button colorScheme="blue" mb={4}>
+      <Link href="/home">Sign In</Link>
+      </Button>
+    <Button colorScheme="green" mb={6}>Register</Button>
+    <Button onClick={toggleColorMode}>Toggle Theme</Button>
+  </Flex>
+</Flex>
+)
+
+  
+
+}
+export default IndexPage
