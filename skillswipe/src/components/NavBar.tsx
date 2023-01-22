b84b68161742d12e417d62879b840d3cff6f8b40
Uzair Ali <uali@wisesystems.com>
2023-01-21 21:59:56 -0500

    Fixed margins for Navbar

--- skillswipe/src/components/NavBar.tsx
+++ skillswipe/src/components/NavBar.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import Link from "next/link";
-import { useState } from "react";
+import {useState} from "react";
 import {
-  useColorMode,
-  Switch,
-  Flex,
-  Button,
-  IconButton,
-  useColorModeValue,
-  Box,
+    useColorMode,
+    Switch,
+    Flex,
+    Button,
+    IconButton,
+    useColorModeValue,
+    Box,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import {HamburgerIcon, CloseIcon} from "@chakra-ui/icons";
 import NextLink from "next/link";
 
 export default function NavBar() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
-  const [display, changeDisplay] = useState("none");
-  const toggleTheme = useColorModeValue("🌙", "💡");
-  const formBackground = useColorModeValue("gray.100", "gray.700");
+    const {colorMode, toggleColorMode} = useColorMode();
+    const isDark = colorMode === "dark";
+    const [display, changeDisplay] = useState("none");
+    const toggleTheme = useColorModeValue("🌙", "💡");
+    const formBackground = useColorModeValue("gray.100", "gray.700");
 
   return (
     <Box as="nav" p={15} w="100%">
       <Flex>
-        <Flex backgroundColor={formBackground}  position="fixed" top="1rem" right="1rem" align="center">
+        <Flex backgroundColor={formBackground}  position="fixed" left="0px" align="center" w="100%">
           {/* Desktop */}
-          <Flex display={["none", "none", "flex", "flex"]}>
+          <Flex display={["none", "none", "flex", "flex"]} m={"auto"}>
             <NextLink href="/home" passHref>
               <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
                 Home
@@ -43,90 +43,90 @@ export default function NavBar() {
               </Button>
             </NextLink>
 
-              <NextLink href="/" passHref>
-                <Button
-                    as="a"
-                    variant="ghost"
-                    aria-label="Sign In/Logout"
-                    my={5}
-                    w="100%"
+                        <NextLink href="/" passHref>
+                            <Button
+                                as="a"
+                                variant="ghost"
+                                aria-label="Sign In/Logout"
+                                my={5}
+                                w="100%"
+                            >
+                                Sign In/Logout
+                            </Button>
+                        </NextLink>
+                    </Flex>
+
+                    {/* Mobile */}
+                    <IconButton
+                        aria-label="Open Menu"
+                        size="lg"
+                        mr={2}
+                        icon={<HamburgerIcon/>}
+                        onClick={() => changeDisplay("flex")}
+                        display={["flex", "flex", "none", "none"]}
+                    />
+                    <Button marginLeft={3} onClick={toggleColorMode}>
+                        {toggleTheme}
+                    </Button>
+                </Flex>
+
+                {/* Mobile Content */}
+                <Flex
+                    w="100vw"
+                    display={display}
+                    bgColor={formBackground}
+                    zIndex={20}
+                    h="100vh"
+                    pos="fixed"
+                    top="0"
+                    left="0"
+                    overflowY="auto"
+                    flexDir="column"
                 >
-                  Sign In/Logout
-                </Button>
-              </NextLink>
+                    <Flex justify="flex-end">
+                        <IconButton
+                            mt={2}
+                            mr={2}
+                            aria-label="Open Menu"
+                            size="lg"
+                            icon={<CloseIcon/>}
+                            onClick={() => changeDisplay("none")}
+                        />
+                    </Flex>
+
+                    <Flex flexDir="column" align="center">
+                        <NextLink href="/home" passHref>
+                            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+                                Home
+                            </Button>
+                        </NextLink>
+
+                        <NextLink href="/inbox" passHref>
+                            <Button
+                                as="a"
+                                variant="ghost"
+                                aria-label="Messages"
+                                my={5}
+                                w="100%"
+                            >
+                                Messages
+                            </Button>
+                        </NextLink>
+
+                        <NextLink href="/" passHref>
+                            <Button
+                                as="a"
+                                variant="ghost"
+                                aria-label="Sign In"
+                                my={5}
+                                w="100%"
+                            >
+                                Sign In/Logout
+                            </Button>
+                        </NextLink>
+                    </Flex>
+                </Flex>
             </Flex>
-
-            {/* Mobile */}
-            <IconButton
-                aria-label="Open Menu"
-                size="lg"
-                mr={2}
-                icon={<HamburgerIcon />}
-                onClick={() => changeDisplay("flex")}
-                display={["flex", "flex", "none", "none"]}
-            />
-            <Button marginLeft={3} onClick={toggleColorMode}>
-              {toggleTheme}
-            </Button>
-          </Flex>
-
-          {/* Mobile Content */}
-          <Flex
-              w="100vw"
-              display={display}
-              bgColor={formBackground}
-              zIndex={20}
-              h="100vh"
-              pos="fixed"
-              top="0"
-              left="0"
-              overflowY="auto"
-              flexDir="column"
-          >
-            <Flex justify="flex-end">
-              <IconButton
-                  mt={2}
-                  mr={2}
-                  aria-label="Open Menu"
-                  size="lg"
-                  icon={<CloseIcon />}
-                  onClick={() => changeDisplay("none")}
-              />
-            </Flex>
-
-            <Flex flexDir="column" align="center">
-              <NextLink href="/home" passHref>
-                <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
-                  Home
-                </Button>
-              </NextLink>
-
-              <NextLink href="/inbox" passHref>
-                <Button
-                    as="a"
-                    variant="ghost"
-                    aria-label="Messages"
-                    my={5}
-                    w="100%"
-                >
-                  Messages
-                </Button>
-              </NextLink>
-
-              <NextLink href="/" passHref>
-                <Button
-                    as="a"
-                    variant="ghost"
-                    aria-label="Sign In"
-                    my={5}
-                    w="100%"
-                >
-                  Sign In/Logout
-                </Button>
-              </NextLink>
-            </Flex>
-          </Flex>
-        </Flex>
-      </Box>
-  );
+        </Box>
+    );
 }
