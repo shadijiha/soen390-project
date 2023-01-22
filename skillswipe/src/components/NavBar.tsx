7e2098b0371482bb0ea609540846c077e31e1e32
Usama Saleem <usama.saleem@hotmail.com>
2023-01-22 16:57:17 -0500

    Update NavBar.tsx

--- skillswipe/src/components/NavBar.tsx
+++ skillswipe/src/components/NavBar.tsx
@@ -42,8 +42,7 @@ export default function NavBar() {
           align="center"
           w="100%"
           backgroundColor="rgba(255, 255, 255, 0.15)"
-          // add a drop shadow
-          boxShadow="0px 0px 10px rgba(0, 0, 0, 0.15)"
+          boxShadow="0px 0px 20px rgba(0, 0, 0, 0.25)"
         >
           {/* Desktop */}
 
