0cf0c5108d7ba8a397b274e1fc22286ba7560da1
Marc-Eid <marc.mn.eid@gmail.com>
2023-01-23 21:52:57 -0500

    added custom decorator @AuthUser to get the logged in user in requests

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -21,6 +21,7 @@ import { User } from "../models/user.entity";
 import { DeleteResult } from "typeorm";
 import { UsersService } from "./users.service";
 import { Users } from "./users.types";
+import { AuthUser, BearerPayload } from "src/util/util";
 
 @Controller("users")
 @ApiTags("Users")
@@ -35,21 +36,20 @@ export class UsersController {
 		return this.usersService.findAll();
 	}
 
-	// @UseGuards(JwtAuthGuard)
-	// @Get("me")
-	// @ApiResponse({ type: Users.GetUserResponse })
-	// async findOneById(@Request() req): Promise<User> {
-	//   return this.usersService.findOneById(req.user.id);
-	// }
+    @Get("me")
+    @ApiResponse({ type: User })
+    public async me(@AuthUser() authedUser: BearerPayload): Promise<User> {
+        return this.usersService.getByEmail(authedUser.email);
+    }
 
 	@Put()
 	@ApiResponse({ type: Users.UpdateUserResponse })
-	update(@Request() req, @Body() user: Users.UpdateUserRequest): Promise<User> {
-		return this.usersService.update(req.user.id, user);
+	update(@AuthUser() authedUser: BearerPayload, @Body() user: Users.UpdateUserRequest): Promise<User> {
+		return this.usersService.update(authedUser.id, user);
 	}
 
 	@Delete()
-	remove(@Request() req): Promise<DeleteResult> {
-		return this.usersService.remove(req.user.id);
+	remove(@AuthUser() authedUser: BearerPayload): Promise<DeleteResult> {
+		return this.usersService.remove(authedUser.id.toString());
 	}
 }
