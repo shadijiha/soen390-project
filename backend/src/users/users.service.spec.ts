b1bc62d01b1b78f9bce0270ffb5143b881f00048
Marc-Eid <marc.mn.eid@gmail.com>
2023-01-24 21:29:23 -0500

    Implemented somel unit tests for users.services

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -6,30 +6,46 @@ import { DataSource, Repository } from "typeorm";
 import { UsersService } from "./users.service";
 
 describe("UsersService", () => {
-    let service: UsersService;
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [
-                UsersService,
-                {
-                    provide: getRepositoryToken(User),
-                    useValue: {},
-                },
-                { provide: DataSource, useFactory: dataSourceMockFactory }
-            ],
-        }).compile();
-        service = module.get<UsersService>(UsersService);
-    });
-    it("should be defined", () => {
-        expect(service).toBeDefined();
-    });
+  let service: UsersService;
+  let userRepository: Repository<User>;
+  let mockUser: User = new User();
+  mockUser.id = 1;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          // define a fake repository that returns the fake users
+          useValue: {
+            find: () => [],
+            findOneBy: () => mockUser,
+          },
+        },
+        { provide: DataSource, useFactory: dataSourceMockFactory },
+      ],
+    }).compile();
+    service = module.get<UsersService>(UsersService);
+    userRepository = module.get(getRepositoryToken(User));
+  });
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
 
-    it('should return all users', async () => {
-        const allUsers = await service.findAll();
-        expect(allUsers.length).toEqual(0);
+  it("should return all users", async () => {
+    const allUsers = await service.findAll();
+    expect(allUsers.length).toEqual(0);
+  });
 
-    });
+  it("should return a user by id", async () => {
+    const user = await service.findOneById(1);
+    expect(user.id).toEqual(1);
+  });
 
+  it("should return a user by id", async () => {
+    const result = await userRepository.findOneBy({id: 1});
+    expect(result).toEqual({ id: 1, username: 'test' });
+  });
 
 
 });
