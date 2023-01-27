76f5c46baf764961f5c05665f50194055170b1a7
Shadi <51339220+shadijiha@users.noreply.github.com>
2023-01-27 16:48:42 -0500

    Generated module for profile editting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
 	imports: [
@@ -22,6 +23,7 @@ import { UsersModule } from "./users/users.module";
 		}),
 		UsersModule,
 		AuthModule,
+		ProfileModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
