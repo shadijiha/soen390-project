79d7f0ae32ac48e85f0e52cbef1a0850c96ec57e
Usama Saleem <usama.saleem@hotmail.com>
2023-01-27 18:21:45 -0500

    Added edit buttons to profile page
    Implementation is not complete, however front end includes edit button.
    
    In addition, removed unnecessary imports in the profile page code file to streamline organization.

--- skillswipe/src/pages/profile.tsx
+++ skillswipe/src/pages/profile.tsx
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from "react";
-import imageFile from "../assets/images/image.jpeg";
 import {
   Box,
   Flex,
   Text,
   Image,
-  Button,
-  Input,
-  FormControl,
-  FormLabel,
-  FormHelperText,
   Stack,
   Divider,
   useColorModeValue,
   useColorMode,
-  VStack,
-  useBreakpointValue,
+  Button,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import Layout from "@/components/Layout";
@@ -26,19 +20,17 @@ var boxBorder = "30";
 
 const Profile = () => {
   useEffect(() => {
-    if(localStorage.getItem('jwt')){
-      checkLogin(localStorage.getItem("jwt")).then((Response) => {
-        setProfile({
-          ...profile,
-          name : Response.data.firstName +" "+Response.data.lastName
+    if (localStorage.getItem("jwt")) {
+      checkLogin(localStorage.getItem("jwt"))
+        .then((Response) => {
+          setProfile({
+            ...profile,
+            name: Response.data.firstName + " " + Response.data.lastName,
+          });
         })
-
-      }).catch((error) => {
-
-      })
-
+        .catch((error) => {});
     }
-  },[])
+  }, []);
   const [profile, setProfile] = useState({
     name: "John Smith",
     title: "Software Engineer",
@@ -48,9 +40,9 @@ const Profile = () => {
     experience2: "Three years of experience in mobile development",
     experience3: "Two years of experience in data analysis",
     image:
-      "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg",
+      "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png?w=640",
     cover:
-      "https://cdn-images.zety.com/pages/software_developer_cover_letter_example_zety_us_4.jpg",
+      "https://www.uidownload.com/files/864/934/332/abstract-background-with-modern-gradient.jpg",
   });
   const { toggleColorMode } = useColorMode();
   const formBorder = useColorModeValue("gray.300", "gray.600");
@@ -83,72 +75,132 @@ const Profile = () => {
       experience2: "Three years of experience in mobile development",
       experience3: "Two years of experience in data analysis",
       image:
-        "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg",
+        "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png?w=640",
       cover:
-        "https://cdn-images.zety.com/pages/software_developer_cover_letter_example_zety_us_4.jpg",
+        "https://media.istockphoto.com/id/931318336/photo/holographic-foil-blurred-abstract-background-for-trendy-design.jpg?s=612x612&w=0&k=20&c=X62G7tOcp3_1IKBPfpwf8ASp99IseNFpTdINcb6RqJA=",
     });
   };
 
   const postBackground = useColorModeValue("gray.100", "gray.700");
+  function setIsEditing(arg0: boolean): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <>
       <Layout>
-        
-      <NavBar />
+        <NavBar />
 
-      <Flex direction="column" align="right" p={12}>
-        <Stack
-          borderWidth={borderWidth}
-          borderColor={formBorder}
-          backgroundColor={postBackground} // we should make this blurred so that the cover shows through a bit
-          padding="0rem"
-          marginBottom="1rem"
-          rounded={boxBorder}
-          overflow="hidden"
-          width="100%"
-          minW="80vw"
-          maxW="90vw"
-          boxShadow="lg"
-          zIndex={"-1"}
-        >
-          <Image
-            src={profile.cover}
-            alt=""
-            w="100%"
-            h="300px"
-            bg="gray.100"
-            rounded="lg"
-            mb={6}
-            objectFit="cover"
-            />
-          <Flex align="center" direction={["column", "column", "row", "row"]}>
+        <Flex direction="column" align="right" p={12}>
+          <Stack
+            borderWidth={borderWidth}
+            borderColor={formBorder}
+            backgroundColor={postBackground} // we should make this blurred so that the cover shows through a bit
+            padding="0rem"
+            marginBottom="1rem"
+            rounded={boxBorder}
+            overflow="hidden"
+            width="100%"
+            minW="80vw"
+            maxW="90vw"
+            boxShadow="lg"
+            zIndex={"-1"}
+          >
             <Image
-              src={profile.image}
+              src={profile.cover}
               alt=""
-              w="150px"
-              h="150px"
-              rounded="full"
-              objectFit={"cover"}
-              mr={6}
+              w="100%"
+              h="300px"
+              bg="gray.100"
+              rounded="lg"
               mb={6}
-              marginLeft={10}
-              boxShadow="lg"
+              objectFit="cover"
+            />
+            <Flex align="center" direction={["column", "column", "row", "row"]}>
+              <Image
+                src={profile.image}
+                alt=""
+                w="150px"
+                h="150px"
+                rounded="full"
+                objectFit={"cover"}
+                mr={6}
+                mb={6}
+                marginLeft={10}
+                boxShadow="lg"
               />
-            <Stack ml={6}>
-              <Text fontSize="2xl" fontWeight="bold">
-                {profile.name}
-              </Text>
-              <Text fontSize="lg" fontWeight="medium">
-                {profile.title}
-              </Text>
-              <Text fontSize="sm">{profile.location}</Text>
-              <Text>{profile.school}</Text>
+              <Stack ml={6}>
+                <Text fontSize="2xl" fontWeight="bold">
+                  {profile.name}
+                </Text>
+                <Text fontSize="lg" fontWeight="medium">
+                  {profile.title}
+                </Text>
+                <Text fontSize="sm">{profile.location}</Text>
+                <Text>{profile.school}</Text>
+              </Stack>
+            </Flex>
+          </Stack>
+          <Divider my={6} w="100%" />
 
-            </Stack>
-          </Flex>
-        </Stack>
-        <Divider my={6} w="100%" />
-        <Stack>
+          <Stack>
+            <Box
+              borderWidth={borderWidth}
+              borderColor={formBorder}
+              backgroundColor={postBackground}
+              padding="1rem"
+              marginBottom="1rem"
+              rounded={boxBorder}
+              overflow="hidden"
+              width="100%"
+              minW="80vw"
+              maxW="90vw"
+              boxShadow="lg"
+            >
+              <SimpleGrid columns={2} spacingX={1} spacingY={1}>
+                <Text
+                  fontSize="lg"
+                  fontWeight="medium"
+                  mb={2}
+                  marginLeft={"1rem"}
+                  display="inline-block"
+                >
+                  Personal Experience
+                </Text>
+                <Button
+                  alignSelf={"flex-end"}
+                  justifyContent={"flex-end"}
+                  variant={"blue"}
+                  size="sm"
+                  onClick={() => setIsEditing(!setIsEditing)}
+                  display="inline-flex"
+                >
+                  Edit
+                </Button>
+              </SimpleGrid>
+
+              <Stack ml={5}>
+                <ol>
+                  <li>
+                    <Text fontSize="sm" marginBottom={"1rem"}>
+                      {profile.experience}
+                    </Text>
+                  </li>
+                  <li>
+                    <Text fontSize="sm" marginBottom={"1rem"}>
+                      {profile.experience2}
+                    </Text>
+                  </li>
+                  <li>
+                    <Text fontSize="sm" marginBottom={"1rem"}>
+                      {profile.experience3}
+                    </Text>
+                  </li>
+                </ol>
+              </Stack>
+            </Box>
+          </Stack>
+
           <Box
             borderWidth={borderWidth}
             borderColor={formBorder}
@@ -161,50 +213,20 @@ const Profile = () => {
             minW="80vw"
             maxW="90vw"
             boxShadow="lg"
-            >
-            <Text fontSize="lg" fontWeight="medium" mb={2} marginLeft={"1rem"}>
-              Personal Experience
-            </Text>
-
-            <Stack ml={5}>
-              <ol>
-                <li>
-                  <Text fontSize="sm" marginBottom={"1rem"}>
-                    {profile.experience}
-                  </Text>
-                </li>
-                <li>
-                  <Text fontSize="sm" marginBottom={"1rem"}>
-                    {profile.experience2}
-                  </Text>
-                </li>
-                <li>
-                  <Text fontSize="sm" marginBottom={"1rem"}>
-                    {profile.experience3}
-                  </Text>
-                </li>
-              </ol>
-            </Stack>
-          </Box>
-        </Stack>
-
-        <Box
-          borderWidth={borderWidth}
-          borderColor={formBorder}
-          backgroundColor={postBackground}
-          padding="1rem"
-          marginBottom="1rem"
-          rounded={boxBorder}
-          overflow="hidden"
-          width="100%"
-          minW="80vw"
-          maxW="90vw"
-          boxShadow="lg"
           >
-          <Text fontSize="lg" fontWeight="medium" mb={2} ml={2}>
-            Employment History:
-          </Text>
-          <Stack>
+            <SimpleGrid columns={2} spacingX={1} spacingY={1}>
+              <Text fontSize="lg" fontWeight="medium" mb={2} ml={2}>
+                Employment History:
+              </Text>
+              <Button
+                justifyContent={"flex-end"}
+                variant={"blue"}
+                size="sm"
+                onClick={() => setIsEditing(!setIsEditing)}
+              >
+                Edit
+              </Button>
+            </SimpleGrid>
             {employmentHistory.map((history, index) => (
               <Box
                 key={index}
@@ -225,10 +247,9 @@ const Profile = () => {
                 <Text>{history.description}</Text>
               </Box>
             ))}
-          </Stack>
-        </Box>
-      </Flex>
-            </Layout>
+          </Box>
+        </Flex>
+      </Layout>
     </>
   );
 };
