f0b5a73cd77f447180cee4a6ae9c1b470d39623c
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-13 12:04:07 -0500

    ran lint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -33,23 +33,19 @@ export class AuthController {
   public async register (
     @Body() body: Auth.RegisterRequest
   ): Promise<void | Auth.LoginResponse> {
-
-    try{
-         const existingUser = await this.userService.findOneByEmail(body.email)
-         if (existingUser) {
-           throw new ConflictException(`Email ${body.email} already taken`)
-         }else{
-            await this.userService.create(body)
-            return await this.authService.login(body)
-         }
-    } catch (e) {
-      console.log("test")
-      console.log(e)
-        // from security point we should not tell to a user that this email was taken already. Message should tell there was an error but not that this email is alrady in the database. Kinda aan easy wau for the hackers.
+    try {
+      const existingUser = await this.userService.findOneByEmail(body.email)
+      if (existingUser != null) {
+        throw new ConflictException(`Email ${body.email} already taken`)
+      } else {
+        await this.userService.create(body)
+        return await this.authService.login(body)
       }
-      
-     
-
+    } catch (e) {
+      console.log('test')
+      console.log(e)
+      // from security point we should not tell to a user that this email was taken already. Message should tell there was an error but not that this email is alrady in the database. Kinda aan easy wau for the hackers.
+    }
   }
 
   @Get('me')
