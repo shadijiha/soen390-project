378c7a501f0b20e1362d3fdb38feaf73bb2d8e62
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-13 22:44:16 -0500

    added endpoint to GET user by id
    added the endpoint to be able to get a single user. important to view user profiles

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,5 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  HttpException,
-  HttpStatus
-} from '@nestjs/common'
-import { Body, Put, Query, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common/decorators'
+import { Controller, Delete, Get, HttpException, HttpStatus } from '@nestjs/common'
+import { Body, Param, Put, Query, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common/decorators'
 import { ApiBearerAuth, ApiConsumes, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { type User } from '../models/user.entity'
@@ -23,10 +17,20 @@ import { FileValidationPipe } from '../util/fileValidationPipe'
 export class UsersController {
   constructor (private readonly usersService: UsersService) {}
 
-  @Get('user')
+  @Get('users')
   @ApiResponse({ type: Users.GetAllUsersResponse })
   async findAll (): Promise<User[]> {
     return await this.usersService.findAll()
+  }
+
+  @Get('user/:id')
+  @ApiResponse({ type: Users.GetAllUsersResponse })
+  async findOne (@Param('id') id: number): Promise<User> {
+    try {
+      return await this.usersService.findOneById(id)
+    } catch (e) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    }
   }
 
   @Delete('user')
@@ -34,19 +38,13 @@ export class UsersController {
     try {
       await this.usersService.removeSoft(authedUser.id)
     } catch (e) {
-      throw new HttpException(
-        'Failed to delete user \n' + (e as Error).message,
-        HttpStatus.PRECONDITION_FAILED
-      )
+      throw new HttpException('Failed to delete user \n' + (e as Error).message, HttpStatus.PRECONDITION_FAILED)
     }
   }
 
   @Get('search')
   @ApiQuery({ name: 'query', required: true })
-  public async search (
-    @AuthUser() authedUser: BearerPayload,
-      @Query('query') query: string
-  ): Promise<Users.SearchResponse> {
+  public async search (@AuthUser() authedUser: BearerPayload, @Query('query') query: string): Promise<Users.SearchResponse> {
     if (query.length <= 0) return { users: [], companies: [] }
     return await this.usersService.search(await authedUser.getUser(), query)
   }
@@ -54,13 +52,17 @@ export class UsersController {
   @Put('user')
   @ApiConsumes('multipart/form-data')
   @ApiResponse({ type: Users.UpdateUserResponse })
-
-  @UseInterceptors(FileFieldsInterceptor([
-    { name: 'profilePic', maxCount: 1 },
-    { name: 'coverPic', maxCount: 1 }
-  ]))
-
-  async update (@AuthUser() authedUser: BearerPayload, @Body() user: Users.UpdateUserRequest, @UploadedFiles(FileValidationPipe) files: { profilePic?: Express.Multer.File, coverPic?: Express.Multer.File }): Promise<User> {
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'profilePic', maxCount: 1 },
+      { name: 'coverPic', maxCount: 1 }
+    ])
+  )
+  async update (
+    @AuthUser() authedUser: BearerPayload,
+      @Body() user: Users.UpdateUserRequest,
+      @UploadedFiles(FileValidationPipe) files: { profilePic?: Express.Multer.File, coverPic?: Express.Multer.File }
+  ): Promise<User> {
     console.log(files)
     return await this.usersService.update(authedUser.id, user, files)
   }
