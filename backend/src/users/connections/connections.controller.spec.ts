83fd9c6a16436762f8a1d1b01c35c34e17996113
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-16 22:05:41 -0500

    fixed connections unit tests

--- backend/src/users/connections/connections.controller.spec.ts
+++ backend/src/users/connections/connections.controller.spec.ts
@@ -1,18 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ConnectionsController } from './connections.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ConnectionsController } from "./connections.controller";
+import { ConnectionsService } from "./connections.service";
 
-describe('ConnectionsController', () => {
+describe("ConnectionsController", () => {
   let controller: ConnectionsController;
+  const mockUserService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ConnectionsController],
-    }).compile();
+      providers: [
+        ConnectionsService
+      ],
+    })
+      .overrideProvider(ConnectionsService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<ConnectionsController>(ConnectionsController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
--- backend/src/users/connections/connections.service.spec.ts
+++ backend/src/users/connections/connections.service.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection } from '../../models/connection.entity';
+import { Repository } from 'typeorm';
 import { ConnectionsService } from './connections.service';
 
 describe('ConnectionsService', () => {
   let service: ConnectionsService;
+  let mockConnectionsRepository =  {
+
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConnectionsService],
+      providers: [
+        ConnectionsService,
+        {
+          provide: getRepositoryToken(Connection),
+          useValue: mockConnectionsRepository
+        }
+        
+      ],
     }).compile();
 
     service = module.get<ConnectionsService>(ConnectionsService);
+   
   });
 
   it('should be defined', () => {
