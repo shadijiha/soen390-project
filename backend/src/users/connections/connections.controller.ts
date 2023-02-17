d30493d5cb2c1d0c8ae064ef101a2499c8769b13
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2023-02-17 13:11:50 -0500

    added delete connection api #60

--- backend/src/users/connections/connections.controller.ts
+++ backend/src/users/connections/connections.controller.ts
@@ -1,88 +1,101 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common/decorators'
-import { HttpException } from '@nestjs/common/exceptions/http.exception'
-import { ApiBearerAuth } from '@nestjs/swagger/dist/decorators/api-bearer.decorator'
-import { ApiTags } from '@nestjs/swagger/dist/decorators/api-use-tags.decorator'
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
-import { AuthUser, BearerPayload } from '../../util/util'
-import { ConnectionsService } from '../../users/connections/connections.service'
-import { Connections } from '../../users/connections/connections.types'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from "@nestjs/common/decorators";
+import { HttpException } from "@nestjs/common/exceptions/http.exception";
+import { ApiBearerAuth } from "@nestjs/swagger/dist/decorators/api-bearer.decorator";
+import { ApiTags } from "@nestjs/swagger/dist/decorators/api-use-tags.decorator";
+import { JwtAuthGuard } from "../../auth/jwt-auth.guard";
+import { AuthUser, BearerPayload } from "../../util/util";
+import { ConnectionsService } from "../../users/connections/connections.service";
+import { Connections } from "../../users/connections/connections.types";
 
-@Controller('connections')
-@ApiTags('Connections')
+@Controller("connections")
+@ApiTags("Connections")
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class ConnectionsController {
-  constructor (private readonly connectionService: ConnectionsService) {
+  constructor(private readonly connectionService: ConnectionsService) {}
 
-  }
-
-  @Post('add')
-  public async sendConnectionRequest (
+  @Post("add")
+  public async sendConnectionRequest(
     @AuthUser() userInfo: BearerPayload,
-      @Body() body: Connections.AddConnectionRequest
+    @Body() body: Connections.AddConnectionRequest
   ): Promise<void> {
     try {
-      await this.connectionService.addConnection(userInfo.id, body.toUser)
+      await this.connectionService.addConnection(userInfo.id, body.toUser);
     } catch (e) {
-      throw new HttpException((e as Error).message, 400)
+      throw new HttpException((e as Error).message, 400);
     }
   }
 
-  @Get('status/:id')
-  public async getConnectionStatus (
+  @Delete("delete/:id")
+  public async removeConnection(
     @AuthUser() userInfo: BearerPayload,
-      @Param('id') user2Id: number
+    @Param("id") id: number
+  ): Promise<any> {
+    try {
+      return await this.connectionService.deleteConnection(userInfo.id, id);
+    } catch (e) {
+      throw new HttpException((e as Error).message, 400);
+    }
+  }
+
+  @Get("status/:id")
+  public async getConnectionStatus(
+    @AuthUser() userInfo: BearerPayload,
+    @Param("id") user2Id: number
   ): Promise<"Connected" | "Pending" | "NotConnected"> {
     try {
-      return await this.connectionService.getConnectionStatus(userInfo.id, user2Id)
+      return await this.connectionService.getConnectionStatus(
+        userInfo.id,
+        user2Id
+      );
     } catch (e) {
-      throw new HttpException((e as Error).message, 400)
+      throw new HttpException((e as Error).message, 400);
     }
   }
 
-  @Get('pending')
-  public async getPendingRequest (
+  @Get("pending")
+  public async getPendingRequest(
     @AuthUser() userInfo: BearerPayload
   ): Promise<any> {
     try {
-      return await this.connectionService.getPendingConnections(userInfo.id)
+      return await this.connectionService.getPendingConnections(userInfo.id);
     } catch (e) {
-      throw new HttpException((e as Error).message, 400)
+      throw new HttpException((e as Error).message, 400);
     }
   }
 
-  @Get('accepted')
-  public async getAcceptedRequests (
+  @Get("accepted")
+  public async getAcceptedRequests(
     @AuthUser() userInfo: BearerPayload
   ): Promise<any> {
     try {
-      return await this.connectionService.getAcceptedConnections(userInfo.id)
+      return await this.connectionService.getAcceptedConnections(userInfo.id);
     } catch (e) {
-      throw new HttpException((e as Error).message, 400)
+      throw new HttpException((e as Error).message, 400);
     }
   }
 
-  @Put('accept')
-  public async acceptConnectionRequest (
+  @Put("accept")
+  public async acceptConnectionRequest(
     @AuthUser() userInfo: BearerPayload,
-      @Body() body: Connections.AcceptConnectionRequest
+    @Body() body: Connections.AcceptConnectionRequest
   ): Promise<any> {
     try {
-      return await this.connectionService.acceptConnection(body.id, userInfo.id)
+      return await this.connectionService.acceptConnection(
+        body.id,
+        userInfo.id
+      );
     } catch (e) {
-      throw new HttpException((e as Error).message, 400)
-    }
-  }
-
-  @Delete('reject')
-  public async rejectConnectionRequest (
-    @AuthUser() userInfo: BearerPayload,
-      @Body() body: Connections.AcceptConnectionRequest
-  ): Promise<any> {
-    try {
-      return await this.connectionService.rejectConnection(body.id, userInfo.id)
-    } catch (e) {
-      throw new HttpException((e as Error).message, 400)
+      throw new HttpException((e as Error).message, 400);
     }
   }
 }
