fa5a50b2842bf344b79dbc518c1030c9065b2a69
Usama Saleem <usama.saleem@hotmail.com>
2023-02-17 16:03:19 -0500

    Made code organized
    Moved education to separate component

--- skillswipe/src/components/Profile/education.tsx
+++ skillswipe/src/components/Profile/education.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
 import {
@@ -18,6 +19,7 @@ import { editEducationHistory } from "@/pages/api/api";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { emailValidator } from "@/Util/Validator";
+import ProfileStyle from "@/styles/profilestyle";
 
 const education = () => {
   // call API to get education history
@@ -26,7 +28,71 @@ const education = () => {
 
   return (
     <>
-      <Text>test</Text>
+      <style jsx>{ProfileStyle}</style>
+      <div className="edu-container">
+        <div className="edu-features">
+          <h1
+            className="edu-text"
+            style={{
+              fontWeight: 600,
+            }}
+          >
+            <span>Education History</span>
+          </h1>
+          <span className="edu-text03">
+            <span>
+              Im a self-taught developer, but I have a degree in Computer
+              Science.
+            </span>
+          </span>
+
+          <div className="edu-container1">
+            <div className="edu-feature-card1">
+              <img
+                src="https://1000logos.net/wp-content/uploads/2017/02/Harvard-Logo.png"
+                className="edu-icon"
+                width="50px"
+                height="50px"
+                alt="Harvard"
+              />
+              <h2 className="edu-text09">Harvard</h2>
+              <span className="edu-text10">
+                TA for CS50, Harvards introductory course to computer science.
+              </span>
+              <span className="edu-text11">2020-Present</span>
+            </div>
+            <div className="edu-feature-card">
+              <img
+                src="https://www.concordia.ca/content/concordia/en/social/guidelines-conduct.img.png/1650398601839.png"
+                className="edu-icon"
+                width="50px"
+                height="50px"
+                alt="Concordia University"
+              />
+              <h2 className="edu-text06">Concordia University</h2>
+              <span className="edu-text07">
+                Earned my bachelors degree in Computer Science, with a minor in
+                Mathematics.
+              </span>
+              <span className="edu-text08">2015-2020</span>
+            </div>
+            <div className="edu-feature-card2">
+              <img
+                src="https://penji.co/wp-content/uploads/2019/02/yale-university-School-Logo-Design-1-975x1024.png"
+                className="edu-icon"
+                width="50px"
+                height="50px"
+                alt="Yale"
+              />
+              <h2 className="edu-text12">Dawson</h2>
+              <span className="edu-text13">
+                Legally forced to complete my high school diploma in my youth
+              </span>
+              <span className="edu-text14">2015</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
