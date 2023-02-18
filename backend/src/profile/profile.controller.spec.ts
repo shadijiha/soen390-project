a879484f562dee344f227fbe70babb9dba8c9e50
Aleksandr Vinokhodov <VinokhodovAlex@gmail.com>
2023-02-17 23:19:26 -0500

    profile controller tests WIP #101

--- backend/src/profile/profile.controller.spec.ts
+++ backend/src/profile/profile.controller.spec.ts
@@ -1,19 +1,28 @@
+import { ExecutionContext } from '@nestjs/common'
+import { createMock } from '@golevelup/ts-jest';
 import { Test, type TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { Award } from '../models/award.entity'
 import { Course } from '../models/course.entity'
 import { Education } from '../models/education.entity'
 import { Language } from '../models/language.entity'
 import { Project } from '../models/project.entity'
 import { Skill } from '../models/skill.entity'
+import { User } from '../models/user.entity'
 import { Volunteering } from '../models/volunteering.entity'
 import { Work } from '../models/work.entity'
 import { ProfileController } from './profile.controller'
 import { ProfileService } from './profile.service'
+import { isGuarded } from '../util/testUtil';
 
 describe('ProfileController', () => {
   let controller: ProfileController
   let service: ProfileService
+
+  let educationRepository: Repository<Education>
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfileController],
@@ -21,68 +30,92 @@ describe('ProfileController', () => {
         ProfileService,
         {
           provide: getRepositoryToken(Education),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Education>>(),
         },
         {
           provide: getRepositoryToken(Course),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Course>>(),
         },
         {
           provide: getRepositoryToken(Project),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Project>>(),
         },
         {
           provide: getRepositoryToken(Volunteering),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Volunteering>>(),
         },
         {
           provide: getRepositoryToken(Award),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Award>>(),
         },
         {
           provide: getRepositoryToken(Language),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Language>>(),
         },
         {
           provide: getRepositoryToken(Skill),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Skill>>(),
         },
         {
           provide: getRepositoryToken(Work),
-          useValue: {
-            find: () => [],
-            softRemove: () => null
-          }
+          useValue: createMock<Repository<Work>>(),
         }
       ]
-    }).compile()
+    }).overrideGuard(JwtAuthGuard) // to bypass JWT auth
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          return true
+        },
+      })
+      .compile()
 
     controller = module.get<ProfileController>(ProfileController)
     service = module.get<ProfileService>(ProfileService)
+    educationRepository = module.get<Repository<Education>>(getRepositoryToken(Education));
+
+    let user: User = new User();
+    user.firstName = 'test'
+    user.lastName = 'test'
+    user.email = 'test@test.ca'
+    user.password = 'test'
+    user.gender = 'male'
+
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it(`should be protected with JwtAuthGuard.`, async () => {
+    expect(isGuarded(ProfileController.prototype.addEducation, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteEducation, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editEdication, JwtAuthGuard)).toBe(true)
+
+    expect(isGuarded(ProfileController.prototype.addProject, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteProject, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editProject, JwtAuthGuard)).toBe(true)
+
+    expect(isGuarded(ProfileController.prototype.addAward, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteAward, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editAward, JwtAuthGuard)).toBe(true)
+
+    expect(isGuarded(ProfileController.prototype.addSkill, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteSkill, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editSkill, JwtAuthGuard)).toBe(true)
+
+    expect(isGuarded(ProfileController.prototype.addLanguage, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteLanguage, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editLanguage, JwtAuthGuard)).toBe(true)
+
+    expect(isGuarded(ProfileController.prototype.addProject, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.deleteProject, JwtAuthGuard)).toBe(true)
+    expect(isGuarded(ProfileController.prototype.editProject, JwtAuthGuard)).toBe(true)
+  })
+
+  it('should successfully post an education to the authenticated user', () => {
+    const education = { institution: 'Concordia', degree: 'Bachelor' };
+
+    // tslint:disable-next-line: no-invalid-await
+    expect(await repo.find()).toEqual([cat]);
   })
 })
--- backend/src/util/testUtil.ts
+++ backend/src/util/testUtil.ts
@@ -1,7 +1,7 @@
-import { type DynamicModule } from '@nestjs/common'
+import { CanActivate, type DynamicModule } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-export function setupTestDB (): [DynamicModule] {
+export function setupTestDB(): [DynamicModule] {
   return [
     TypeOrmModule.forRoot({
       type: 'sqlite',
@@ -14,4 +14,37 @@ export function setupTestDB (): [DynamicModule] {
       synchronize: true
     })
   ]
+}
+
+/**
+ * Checks whether a route or a Controller is protected with the specified Guard.
+ * @param route is the route or Controller to be checked for the Guard.
+ * @param guardType is the type of the Guard, e.g. JwtAuthGuard.
+ * @returns true if the specified Guard is applied.
+ */
+export function isGuarded(
+  route: ((...args: any[]) => any) | (new (...args: any[]) => unknown),
+  guardType: new (...args: any[]) => CanActivate
+) {
+  const guards: any[] = Reflect.getMetadata('__guards__', route)
+
+  if (!guards) {
+    throw Error(
+      `Expected: ${route.name} to be protected with ${guardType.name}\nReceived: No guard`
+    )
+  }
+
+  let foundGuard = false
+  const guardList: string[] = []
+  guards.forEach((guard) => {
+    guardList.push(guard.name)
+    if (guard.name === guardType.name) foundGuard = true
+  })
+
+  if (!foundGuard) {
+    throw Error(
+      `Expected: ${route.name} to be protected with ${guardType.name}\nReceived: only ${guardList}`
+    )
+  }
+  return true
 }
