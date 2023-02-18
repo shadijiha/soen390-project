e839ea9b1b5aa082e30297ec56e0f6b549c7dc84
Aleksandr Vinokhodov <VinokhodovAlex@gmail.com>
2023-02-17 20:08:49 -0500

    make tests build for profile module #101

--- backend/src/profile/profile.controller.spec.ts
+++ backend/src/profile/profile.controller.spec.ts
@@ -1,4 +1,13 @@
 import { Test, type TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Award } from '../models/award.entity'
+import { Course } from '../models/course.entity'
+import { Education } from '../models/education.entity'
+import { Language } from '../models/language.entity'
+import { Project } from '../models/project.entity'
+import { Skill } from '../models/skill.entity'
+import { Volunteering } from '../models/volunteering.entity'
+import { Work } from '../models/work.entity'
 import { ProfileController } from './profile.controller'
 import { ProfileService } from './profile.service'
 
@@ -9,7 +18,63 @@ describe('ProfileController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfileController],
       providers: [
-        ProfileService
+        ProfileService,
+        {
+          provide: getRepositoryToken(Education),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Course),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Project),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Volunteering),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Award),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Language),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Skill),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        },
+        {
+          provide: getRepositoryToken(Work),
+          useValue: {
+            find: () => [],
+            softRemove: () => null
+          }
+        }
       ]
     }).compile()
 
