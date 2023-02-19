b93f37ba92f11b31c3f426c11b789b42b6d1f2c9
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2023-02-19 13:58:09 -0500

    Backend use env var for port
    Since preparing for the deployment to cloud we need to be able to change the port depending on the machine or container where we deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap (): Promise<void> {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
-  await app.listen(8080)
+  await app.listen(process.env.PORT ?? 8080)
 }
 void bootstrap()
