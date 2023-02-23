1dc3eb95d4ccaaec31868e0072085a310b0a04dc
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2023-02-23 14:18:33 -0500

    Back run lint

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
-async function bootstrap(): Promise<void> {
+async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
   process.env.NODE_ENV !== 'development'
     ? app.enableCors({
