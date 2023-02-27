f3ca342197b65a3f7531064c2e357abae4cf88b8
Uzair Ali <uzairali245@gmail.com>
2023-02-26 19:52:41 -0500

    fixed navbar test by adding provoder tag

--- skillswipe/__tests__/Navbar.test.tsx
+++ skillswipe/__tests__/Navbar.test.tsx
@@ -3,15 +3,25 @@ import NavBar from '@/components/NavBar'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from '../src/Redux/store'
 
 jest.mock('next/router', () => require('next-router-mock'))
-describe('Home', () => {
-  const renderHome = () => render(<NavBar />)
-  beforeAll(() => {
-    ;<Layout></Layout>
-  })
+describe('NavBar', () => {
+  const renderNavBar = () =>
+    render(
+      <Provider store={store}>
+        {' '}
+        <Layout>
+          <NavBar />
+        </Layout>
+      </Provider>
+    )
+  // beforeAll(() => {
+  //   ;<Layout></Layout>
+  // })
   it('should render NavBar without crashing', () => {
-    renderHome()
+    renderNavBar()
     expect(screen.queryByTestId('Nav-Bar'))
   })
 })
