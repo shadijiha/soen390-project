f8b12566f3523c95a32ddded37d01339fa4c9a8c
Uzair Ali <uzairali245@gmail.com>
2023-02-27 01:56:28 -0500

    added test for Job components

--- skillswipe/__tests__/FindJob.test.tsx
+++ skillswipe/__tests__/FindJob.test.tsx
@@ -1,27 +1,29 @@
 import Layout from '@/components/Layout'
-import FindJob from '@/pages/findJob'
+import JobListing from '@/pages/jobListing'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../src/Redux/store'
 
 jest.mock('next/router', () => require('next-router-mock'))
-describe('Jobs', () => {
-  const renderJobs = () =>
+describe('JobListing', () => {
+  const renderJobListing = () =>
     render(
       <Provider store={store}>
         {' '}
         <Layout>
-          <FindJob />
+          <JobListing />
         </Layout>
       </Provider>
     )
   //   beforeAll(()=>{
   //     <Layout></Layout>
   // })
-  it('should render find job page without crashing', () => {
-    renderJobs()
-    expect(screen.queryByTestId('find-jobs'))
+  it('should render JobListing page without crashing', async () => {
+    renderJobListing()
+    await waitFor(() => {
+      expect(screen.getByTestId('job-listing')).toBeInTheDocument()
+    })
   })
 })
