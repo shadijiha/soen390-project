5ebc0e9fc923d48cb0079891542e155e1aaa4e3c
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-28 11:33:34 -0500

    added cascade to jobs relation in recruiter entity

--- backend/src/models/user_types/recruiter.entity.ts
+++ backend/src/models/user_types/recruiter.entity.ts
@@ -6,7 +6,7 @@ import { User } from "../user.entity";
 
 @ChildEntity()
 export class Recruiter extends User {
-  @OneToMany(() => Job, (j) => j.recruiter)
+  @OneToMany(() => Job, (j) => j.recruiter, { cascade: true, orphanedRowAction: 'delete' })
   @ApiProperty({ type: [Job] })
   jobs: Job[];
 }
