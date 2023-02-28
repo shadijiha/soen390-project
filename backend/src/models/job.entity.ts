2167f445e250b4ae1867cf792a3cc58e4278c55f
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-28 00:52:20 -0500

    added job entity to db
    missing skills from job description

---
+++ backend/src/models/job.entity.ts
@@ -0,0 +1,66 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Skill } from "./skill.entity";
+import { Recruiter } from "./user_types/recruiter.entity";
+
+@Entity()
+export class Job extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
+
+  @Column()
+  @ApiProperty()
+  jobTitle: string;
+
+  @Column()
+  @ApiProperty()
+  companyName: string;
+
+  @Column()
+  @ApiProperty()
+  location: string;
+
+  @Column()
+  @ApiProperty()
+  jobDescription: Date;
+
+
+  @Column()
+  @ApiProperty()
+  salary: number;
+
+  @Column()
+  @ApiProperty()
+  jobType: "full-time" | "part-time" | "internship" | "contract" | "temporary" | "volunteer" | "other";
+
+  @Column()
+  @ApiProperty()
+  startDate: Date;
+
+  @CreateDateColumn()
+  @ApiProperty()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  @ApiProperty()
+  updated_at: Date;
+
+  // RELATIONS
+
+  // recruiter
+  @ManyToOne(() => Recruiter, (r) => r.jobs)
+  @ApiProperty({ type: Recruiter })
+  recruiter: Recruiter;
+
+
+  // skills
+  // @ManyToMany((type) => Skill, (skill) => skill.job, {
+  //   cascade: true,
+  //   orphanedRowAction: "delete",
+  // })
+  // @JoinTable()
+  // @ApiProperty({ type: [Skill] })
+  // skills: Skill[];
+
+}
--- backend/src/models/user_types/recruiter.entity.ts
+++ backend/src/models/user_types/recruiter.entity.ts
@@ -1,5 +1,12 @@
-import { ChildEntity } from 'typeorm'
-import { User } from '../user.entity'
+import { ApiProperty } from "@nestjs/swagger";
+import { ChildEntity, OneToMany } from "typeorm";
+import { Education } from "../education.entity";
+import { Job } from "../job.entity";
+import { User } from "../user.entity";
 
 @ChildEntity()
-export class Recruiter extends User {}
+export class Recruiter extends User {
+  @OneToMany(() => Job, (j) => j.recruiter)
+  @ApiProperty({ type: [Job] })
+  jobs: Job[];
+}
