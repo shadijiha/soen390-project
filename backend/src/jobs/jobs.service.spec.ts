f549e05798cc621b5b64078d3b5d15755cbf6ba2
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-28 22:50:35 -0500

    added unit tests for jobs.service to a 100%

--- backend/src/jobs/jobs.service.spec.ts
+++ backend/src/jobs/jobs.service.spec.ts
@@ -1,27 +1,38 @@
-import { createMock } from '@golevelup/ts-jest';
-import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Job } from '../models/job.entity';
-import { Repository } from 'typeorm';
-import { Recruiter } from '../models/user_types/recruiter.entity';
-import { JobsService } from './jobs.service';
+import { createMock } from "@golevelup/ts-jest";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Job } from "../models/job.entity";
+import { RemoveOptions, Repository, SaveOptions } from "typeorm";
+import { Recruiter } from "../models/user_types/recruiter.entity";
+import { JobsService } from "./jobs.service";
+import { Jobs } from "./jobs.types";
+import { Message } from "src/models/message.entity";
+import { User } from "src/models/user.entity";
 
-
-describe('JobsService', () => {
+describe("JobsService", () => {
   let service: JobsService;
-  const recruiterRepository = createMock<Repository<Recruiter>>()
-  const jobsRepository = createMock<Repository<Job>>()
+  let mockRecruiterRepository = {
+    save: jest.fn(),
+    update: jest.fn(),
+  };
+
+  let mockjobsRepository = {
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JobsService,
+      providers: [
+        JobsService,
         {
           provide: getRepositoryToken(Recruiter),
-          useValue: recruiterRepository
+          useValue: mockRecruiterRepository,
         },
         {
           provide: getRepositoryToken(Job),
-          useValue: jobsRepository
+          useValue: mockjobsRepository,
         },
       ],
     }).compile();
@@ -29,7 +40,106 @@ describe('JobsService', () => {
     service = module.get<JobsService>(JobsService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
+
+  it("should create a job", async () => {
+    let data: Jobs.AddJobRequest = {
+      jobTitle: "Software Engineer",
+      skills: "Java,C++,Python",
+    } as unknown as Jobs.AddJobRequest;
+
+    let recruiter: Recruiter = {
+      id: 1,
+      jobs: [],
+    } as unknown as Recruiter;
+
+    await service.createJob(data, recruiter);
+
+    expect(mockRecruiterRepository.save).toHaveBeenCalled();
+
+    data = {
+      jobTitle: "Software Engineer",
+      skills: '',
+    } as unknown as Jobs.AddJobRequest;
+    
+    await service.createJob(data, recruiter);
+    expect(mockRecruiterRepository.save).toHaveBeenCalled();
+
+  });
+
+  it("should update a job", async () => {
+    let jobId = 1;
+    let data: Jobs.UpdateJobRequest = {
+      jobTitle: "Software Engineer",
+      skills: "Java,C++,Python",
+    } as unknown as Jobs.UpdateJobRequest;
+
+    let recruiter: Recruiter = {
+      id: 1,
+      jobs: [],
+    } as unknown as Recruiter;
+
+    try {
+      await service.updateJob(jobId, data, recruiter);
+    } catch (e) {
+      expect(e.message).toBe("Not Found");
+    }
+
+    recruiter = {
+      id: 1,
+      jobs: [
+        {
+          id: 1,
+        },
+      ],
+    } as unknown as Recruiter;
+
+    await service.updateJob(jobId, data, recruiter);
+    expect(mockjobsRepository.save).toHaveBeenCalled();
+    expect(mockjobsRepository.update).toHaveBeenCalled();
+
+    data = {
+      jobTitle: "Software Engineer",
+      skills: "",
+    } as unknown as Jobs.UpdateJobRequest;
+
+    await service.updateJob(jobId, data, recruiter);
+    expect(mockjobsRepository.save).toHaveBeenCalled();
+
+
+  });
+
+  it("should delete a job", async () => {
+    let jobId = 1;
+    let recruiter: Recruiter = {
+      id: 1,
+      jobs: [],
+    } as unknown as Recruiter;
+
+
+    try {
+      await service.deleteJob(jobId, recruiter);
+    }
+    catch (e) {
+      expect(e.message).toBe("Not Found");
+    }
+
+    recruiter = {
+      id: 1,
+      jobs: [
+        {
+          id: 1,
+        },
+      ],
+    } as unknown as Recruiter;
+
+    await service.deleteJob(jobId, recruiter);
+    expect(mockjobsRepository.delete).toHaveBeenCalled();
+
+  
+  });
+
+
 });
--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -1,87 +1,87 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Job } from '../models/job.entity'
-import { Skill } from '../models/skill.entity'
-import { Recruiter } from '../models/user_types/recruiter.entity'
-import { Repository } from 'typeorm'
-import { type Jobs } from './jobs.types'
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Job } from "../models/job.entity";
+import { Skill } from "../models/skill.entity";
+import { Recruiter } from "../models/user_types/recruiter.entity";
+import { Repository } from "typeorm";
+import { type Jobs } from "./jobs.types";
 
 @Injectable()
 export class JobsService {
-  constructor (
+  constructor(
     @InjectRepository(Recruiter)
     private readonly recruiterRepository: Repository<Recruiter>,
     @InjectRepository(Job)
     private readonly jobsRepository: Repository<Job>
   ) {}
 
-  async createJob (data: Jobs.AddJobRequest, recruiter: Recruiter): Promise<void> {
-    const job = new Job()
-    job.jobTitle = data.jobTitle
-    job.companyName = data.companyName
-    job.location = data.location
-    job.jobDescription = data.jobDescription
-    job.salary = data.salary
-    job.jobType = data.jobType
-    job.startDate = data.startDate
-    job.coverLetter = data.coverLetter
-    job.transcript = data.transcript
+  async createJob(data: Jobs.AddJobRequest, recruiter: Recruiter): Promise<void> {
+    const job = new Job();
+    job.jobTitle = data.jobTitle;
+    job.companyName = data.companyName;
+    job.location = data.location;
+    job.jobDescription = data.jobDescription;
+    job.salary = data.salary;
+    job.jobType = data.jobType;
+    job.startDate = data.startDate;
+    job.coverLetter = data.coverLetter;
+    job.transcript = data.transcript;
 
-    const skills: Skill[] = []
+    const skills: Skill[] = [];
     data.skills
-      .split(',')
-      .filter((s) => s !== '')
+      .split(",")
+      .filter((s) => s !== "")
       .forEach((s: string, i: number): void => {
-        skills[i] = new Skill()
-        skills[i].title = s
-      })
+        skills[i] = new Skill();
+        skills[i].title = s;
+      });
 
-    if (skills.length === 0) return
+    if (skills.length === 0) return;
 
-    job.skills = [...skills]
+    job.skills = [...skills];
 
-    recruiter.jobs = [...recruiter.jobs, job]
+    recruiter.jobs = [...recruiter.jobs, job];
 
-    await this.recruiterRepository.save(recruiter)
+    await this.recruiterRepository.save(recruiter);
   }
 
-  async updateJob (jobId: number, data: Jobs.UpdateJobRequest, recruiter: Recruiter): Promise<void> {
-    const found = recruiter.jobs.find((job) => job.id === jobId)
+  async updateJob(jobId: number, data: Jobs.UpdateJobRequest, recruiter: Recruiter): Promise<void> {
+    const found = recruiter.jobs.find((job) => job.id === jobId);
 
     if (found == null) {
-      throw new NotFoundException()
+      throw new NotFoundException();
     }
 
-    const job: Job = found
+    const job: Job = found;
 
     if (data.skills != null) {
-      const jobSkills: Skill[] = []
+      const jobSkills: Skill[] = [];
       data.skills
-        .split(',')
-        .filter((s) => s !== '')
+        .split(",")
+        .filter((s) => s !== "")
         .forEach((s: string, i: number): void => {
-          jobSkills[i] = new Skill()
-          jobSkills[i].title = s
-        })
+          jobSkills[i] = new Skill();
+          jobSkills[i].title = s;
+        });
 
-      if (jobSkills.length === 0) return
+      if (jobSkills.length === 0) return;
 
-      job.skills = [...jobSkills]
+      job.skills = [...jobSkills];
 
-      await this.jobsRepository.save(job)
+      await this.jobsRepository.save(job);
     }
-    const { skills, ...dataNoSkills } = data
+    const { skills, ...dataNoSkills } = data;
 
-    await this.jobsRepository.update(jobId, dataNoSkills)
+    await this.jobsRepository.update(jobId, dataNoSkills);
   }
 
-  async deleteJob (jobId: number, recruiter: Recruiter): Promise<void> {
-    const found = recruiter.jobs.find((job) => job.id === jobId)
+  async deleteJob(jobId: number, recruiter: Recruiter): Promise<void> {
+    const found = recruiter.jobs.find((job) => job.id === jobId);
 
     if (found == null) {
-      throw new NotFoundException()
+      throw new NotFoundException();
     }
 
-    await this.jobsRepository.delete(found.id)
+    await this.jobsRepository.delete(found.id);
   }
 }
