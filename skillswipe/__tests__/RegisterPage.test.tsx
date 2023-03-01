53799e2998a37d2e0dfc4291ff7a428b5628b88e
Usama Saleem <usama.saleem@hotmail.com>
2023-03-01 13:19:58 -0500

    Improved testing coverage

--- skillswipe/__tests__/RegisterPage.test.tsx
+++ skillswipe/__tests__/RegisterPage.test.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
 import Register from '@/pages/register'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../src/Redux/store'
@@ -21,8 +21,10 @@ describe('Register', () => {
   // beforeAll(()=>{
   //     <Layout></Layout>
   // })
-  it('should render register page without crashing', () => {
+  it('should render register page without crashing', async () => {
     renderSignup()
-    expect(screen.queryByTestId('register-page'))
+    await waitFor(() => {
+      expect(screen.queryByTestId('register-page')).toBeInTheDocument()
+    })
   })
 })
