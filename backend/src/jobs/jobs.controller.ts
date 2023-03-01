29ae4a07e658b80554f95dcb55859fc89413ec7c
Marc-Eid <marc.mn.eid@gmail.com>
2023-02-28 20:40:46 -0500

    unit tests of jobs.controller at 100%

--- backend/src/jobs/jobs.controller.ts
+++ backend/src/jobs/jobs.controller.ts
@@ -12,25 +12,27 @@ import { Jobs } from './jobs.types'
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class JobsController {
-  constructor (private readonly usersService: JobsService) {}
+  constructor (private readonly jobsService: JobsService) {}
 
   // create a job post only allowed if user is a recruiter
   @Post()
   async createJob (@AuthUser() authedUser: BearerPayload, @Body() job: Jobs.AddJobRequest): Promise<void> {
     const recruiter: Recruiter = (await authedUser.getUser(['jobs'])) as Recruiter
+    console.log(recruiter)
     if (recruiter == null) {
       return
     }
 
     if (recruiter.type !== 'recruiter') throw new HttpException('Only recruiters can create job posts', 400)
 
-    await this.usersService.createJob(job, recruiter)
+    await this.jobsService.createJob(job, recruiter)
   }
 
   // get all jobs listings for a recruiter
   @Get()
   async getJobs (@AuthUser() authedUser: BearerPayload): Promise<Job[]> {
     const recruiter: Recruiter = (await authedUser.getUser(['jobs'])) as Recruiter
+    
     if (recruiter == null) {
       throw new HttpException('Recruiters does not exist', 400)
     }
@@ -49,7 +51,7 @@ export class JobsController {
     }
 
     if (recruiter.type !== 'recruiter') throw new HttpException('Only recruiters can update job posts', 400)
-    await this.usersService.updateJob(parseInt(id), job, recruiter)
+    await this.jobsService.updateJob(parseInt(id), job, recruiter)
   }
 
   @Delete(':id')
@@ -60,6 +62,6 @@ export class JobsController {
     }
 
     if (recruiter.type !== 'recruiter') throw new HttpException('Only recruiters can delete job posts', 400)
-    await this.usersService.deleteJob(parseInt(id), recruiter)
+    await this.jobsService.deleteJob(parseInt(id), recruiter)
   }
 }
