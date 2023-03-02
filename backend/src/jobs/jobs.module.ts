69b77bf729c9e8b4039c32b78b978cb583800823
Marc-Eid <marc.mn.eid@gmail.com>
2023-03-02 10:14:13 -0500

    aligned import paths with code base

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common'
 import { JobsService } from './jobs.service'
 import { JobsController } from './jobs.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { Job } from 'src/models/job.entity'
-import { Recruiter } from 'src/models/user_types/recruiter.entity'
-import { Skill } from 'src/models/skill.entity'
+import { Job } from '../models/job.entity'
+import { Recruiter } from '../models/user_types/recruiter.entity'
+import { Skill } from '../models/skill.entity'
 
 @Module({
   providers: [JobsService],
