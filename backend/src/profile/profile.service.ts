bb6bc077706d864e879a3d81b3f16d87b85bf94f
Marc-Eid <marc.mn.eid@gmail.com>
2023-03-02 14:29:11 -0500

    fixed the language attribute logic #185

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -182,25 +182,37 @@ export class ProfileService {
     language.languageName = data.languageName
     language.proficiency = data.proficiency
 
-    user.languages = [...user.languages, language]
+    const existingLanguage = await this.languageRepository.findOne({
+      where: {
+        languageName: data.languageName,
+        proficiency: data.proficiency
+      }
+    })
+
+    if (existingLanguage != null) {
+      user.languages = [...user.languages, existingLanguage]
+    } else {
+      user.languages = [...user.languages, language]
+    }
+
     await user.save()
   }
 
   public async removeLanguage (user: User, id: number): Promise<void> {
-    await this.languageRepository
-      .findOneOrFail({
-        where: [{ id }, { user: { id: user.id } }]
-      })
-      .then(async (l: Language) => await this.languageRepository.delete({ id: l.id }))
+    user.languages = user.languages.filter((s) => s.id !== id)
+
+    await user.save()
   }
 
   public async editLanguage (user: User, request: Profile.EditLanguageRequest): Promise<void> {
-    const found = user.languages.find((l) => l.id === request.id)
-    if (found == null) {
-      throw new NotFoundException()
-    }
-    const language: Language = found
-    await this.languageRepository.update(language.id, request)
+    // const found = user.languages.find((l) => l.id === request.id);
+    // if (found == null) {
+    //   throw new NotFoundException();
+    // }
+    // const language: Language = found;
+    // await this.languageRepository.update(language.id, request);
+    await this.removeLanguage(user, request.id)
+    await this.addLanguage(user, request)
   }
 
   public async addSkill (user: User, data: Profile.AddSkillRequest): Promise<void> {
