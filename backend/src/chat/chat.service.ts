69eff9260bd978a563ddd60f4a93c510a61f9bfe
Aleksandr Vinokhodov <VinokhodovAlex@gmail.com>
2023-03-06 19:06:13 -0500

    Back: add node env to chat service #176 #69

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -10,10 +10,10 @@ export class ChatService {
   constructor (private readonly messageRepository: Repository<Message>) {
     // TODO: These should be in a .env file
     this.pusher = new Pusher({
-      appId: 'APP_ID',
-      key: 'APP_KEY',
-      secret: 'APP_SECRET',
-      cluster: 'APP_CLUSTER',
+      appId: process.env.PUSHER_APP_ID,
+      key: process.env.PUSHER_APP_KEY,
+      secret: process.env.PUSHER_APP_SECRET,
+      cluster: process.env.PUSHER_APP_CLUSTER,
       useTLS: true,
       encrypted: true
     })
