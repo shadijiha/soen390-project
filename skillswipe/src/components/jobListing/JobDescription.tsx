7c36e2e9303c59ad396cc036a5a482785a606e31
Usama Saleem <usama.saleem@hotmail.com>
2023-03-10 14:52:39 -0500

    Update JobDescription.tsx
    Added jobDescription to API

--- skillswipe/src/components/jobListing/JobDescription.tsx
+++ skillswipe/src/components/jobListing/JobDescription.tsx
@@ -1,34 +1,38 @@
 import { Flex, Text } from '@chakra-ui/react'
 
-const JobDescription = () => {
+interface jobDescriptionProps {
+  jobDescription?: string
+}
+
+const JobDescription = ({ jobDescription }: jobDescriptionProps) => {
+  const data = [{ id: 1, text: `${jobDescription}`, title: 'Job Description' }]
+
   return (
     <>
-      <Flex align="left" direction="column">
-        <Text
-          mx={{ base: 25, sm: 3, md: 130 }}
-          style={{
-            fontWeight: 600,
-            fontSize: '2rem',
-            textShadow: '0px 0px 10px #00000010',
-            paddingBottom: '0.5em',
-          }}
-        >
-          Description
-        </Text>
-        <Text
-          fontSize="18px"
-          textAlign="justify"
-          fontFamily="roboto"
-          paddingBottom={1}
-          mx={{ base: 25, sm: 3, md: 130 }}
-        >
-          As a Google Software Engineer Intern, youll work on our core products and
-          services, gaining real-world experience and working on projects that impact
-          millions of users. Youll have the opportunity to collaborate with
-          experienced engineers and designers, contribute to open-source projects,
-          and develop new features that will be used by people all around the world.
-        </Text>
-      </Flex>
+      {data.map((job) => (
+        <Flex key={job.id} align="left" direction="column">
+          <Text
+            mx={{ base: 25, sm: 3, md: 130 }}
+            style={{
+              fontWeight: 600,
+              fontSize: '2rem',
+              textShadow: '0px 0px 10px #00000010',
+              paddingBottom: '0.5em',
+            }}
+          >
+            {job.title}
+          </Text>
+          <Text
+            fontSize="18px"
+            textAlign="justify"
+            fontFamily="roboto"
+            paddingBottom={1}
+            mx={{ base: 25, sm: 3, md: 130 }}
+          >
+            {job.text}
+          </Text>
+        </Flex>
+      ))}
     </>
   )
 }
