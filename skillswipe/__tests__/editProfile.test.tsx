d2ac221a84f47e0027da4ea01a401b0083bfa4b4
Uzair Ali <uzairali245@gmail.com>
2023-03-10 14:27:39 -0500

    edit email validator import in informationBox

--- skillswipe/__tests__/editProfile.test.tsx
+++ skillswipe/__tests__/editProfile.test.tsx
@@ -1,24 +1,24 @@
+import Layout from '@/components/Layout'
+import EditProfile from '@/pages/editProfile'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../src/Redux/store'
-import Layout from '@/components/Layout'
-import EditProfile from '@/pages/editProfile'
 
 jest.mock('next/router', () => require('next-router-mock'))
 describe('edit-profile', () => {
-    const renderEditProfile = () =>
-      render(
-        <Provider store={store}>
-          {' '}
-          <Layout>
-            <EditProfile/>
-          </Layout>
-        </Provider>
-      )
-    it('should render courses page without crashing', () => {
-      renderEditProfile()
-      expect(screen.queryByTestId('edit-profile'))
-    })
+  const renderEditProfile = () =>
+    render(
+      <Provider store={store}>
+        {' '}
+        <Layout>
+          <EditProfile />
+        </Layout>
+      </Provider>
+    )
+  it('should render courses page without crashing', () => {
+    renderEditProfile()
+    expect(screen.queryByTestId('edit-profile'))
   })
+})
