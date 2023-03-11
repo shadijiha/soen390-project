32ef406ba6f8cbd478143e55c9645dae67178178
Uzair Ali <uzairali245@gmail.com>
2023-03-10 23:38:20 -0500

    updated edit profile test

--- skillswipe/__tests__/editProfile.test.tsx
+++ skillswipe/__tests__/editProfile.test.tsx
@@ -1,24 +1,88 @@
+// import Layout from '@/components/Layout'
+// import EditProfile from '@/pages/editProfile'
+// import '@testing-library/jest-dom'
+// import { render, screen, waitFor } from '@testing-library/react'
+// import React from 'react'
+// import { Provider } from 'react-redux'
+// import store from '../src/Redux/store'
+
+// jest.mock('next/router', () => require('next-router-mock'))
+// describe('edit-profile', () => {
+//   const renderEditProfile = () =>
+//     render(
+//       <Provider store={store}>
+//         {' '}
+//         <Layout>
+//           <EditProfile />
+//         </Layout>
+//       </Provider>
+//     )
+//   it('should render courses page without crashing', async () => {
+//     renderEditProfile()
+//     await waitFor(() => {
+//       expect(screen.getByTestId('edit-profile')).toBeInTheDocument()
+//     })
+//   })
+// })
+import Layout from '@/components/Layout'
+import Register from '@/pages/register'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+
 import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../src/Redux/store'
-import Layout from '@/components/Layout'
-import EditProfile from '@/pages/editProfile'
 
 jest.mock('next/router', () => require('next-router-mock'))
-describe('edit-profile', () => {
-    const renderEditProfile = () =>
-      render(
-        <Provider store={store}>
-          {' '}
-          <Layout>
-            <EditProfile/>
-          </Layout>
-        </Provider>
-      )
-    it('should render courses page without crashing', () => {
-      renderEditProfile()
-      expect(screen.queryByTestId('edit-profile'))
+
+describe('Register', () => {
+  const renderSignup = () =>
+    render(
+      <Provider store={store}>
+        {' '}
+        <Layout>
+          <Register />
+        </Layout>
+      </Provider>
+    )
+  // beforeAll(()=>{
+  //     <Layout></Layout>
+  // })
+  it('should render register page without crashing', async () => {
+    renderSignup()
+    await waitFor(() => {
+      expect(screen.getByTestId('register-page')).toBeInTheDocument()
     })
   })
+  it('should check inputs for register page', async () => {
+    renderSignup()
+    await waitFor(() => {
+      const fname = screen.getByTestId('first-name')
+      const lname = screen.getByTestId('last-name')
+      const email = screen.getByTestId('email')
+      const pass = screen.getByTestId('password')
+      const confirmPass = screen.getByTestId('confirm-password')
+      fireEvent.change(fname, {
+        target: { value: 'test' },
+      })
+      fireEvent.change(lname, {
+        target: { value: 'testing' },
+      })
+      fireEvent.change(email, {
+        target: { value: 'testing@gmail.com' },
+      })
+      fireEvent.change(pass, {
+        target: { value: '1234567' },
+      })
+      fireEvent.change(confirmPass, {
+        target: { value: '1234567' },
+      })
+
+      expect(screen.getByTestId('first-name')).toHaveValue('test')
+      expect(screen.getByTestId('last-name')).toHaveValue('testing')
+      expect(screen.getByTestId('email')).toHaveValue('testing@gmail.com')
+      expect(screen.getByTestId('password')).toHaveValue('1234567')
+      expect(screen.getByTestId('confirm-password')).toHaveValue('1234567')
+    })
+  })
+})
