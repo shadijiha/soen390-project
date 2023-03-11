ace1e955bfe56f150aa105d9e0528654bff6e5e1
Uzair Ali <uzairali245@gmail.com>
2023-03-11 13:13:42 -0500

    render test for inbox page

--- skillswipe/src/pages/inbox/index.tsx
+++ skillswipe/src/pages/inbox/index.tsx
@@ -27,67 +27,75 @@ const Inbox = () => {
   const [loading, setLoading] = useState(true)
   const User = useSelector((state) => state as any)
   useEffect(() => {
-    if(User.auth){
-      const token = localStorage.getItem("jwt")
-      getAllConversation(token).then((response) => {
-        var allConvo = response.data;
-        allConvo = allConvo.filter(filterConvo)
-        setMessages(allConvo);    
-        setLoading(false);
-      }).catch((error) => {
-        toast(error.message)
-      })
+    if (User.auth) {
+      const token = localStorage.getItem('jwt')
+      getAllConversation(token)
+        .then((response) => {
+          var allConvo = response.data
+          allConvo = allConvo.filter(filterConvo)
+          setMessages(allConvo)
+          setLoading(false)
+        })
+        .catch((error) => {
+          toast(error.message)
+        })
     }
-  },[User.auth])
-  const filterConvo = (element : any) =>{
+  }, [User.auth])
+  const filterConvo = (element: any) => {
     return element.id != User.auth.id
-
   }
   return (
     <>
-      <Layout>
-        <NavBar></NavBar>
-          {
-            loading == true ? 
-            <Spinner/>
-            :
-            <Box p={50}>
-          <Heading as="h1" size="lg" mb={4}>
-            Inbox
-          </Heading>
-          {messages.length > 0 ? (
-            messages.map((element : any) => (
-              <Flex
-                key={element.id}
-                borderWidth="1px"
-                borderRadius="lg"
-                p={4}
-                mb={4}
-                display="flex"
-                alignItems="center"
-                cursor={'pointer'}
-                onClick={() => router.push(`/inbox/${element.id}`)}
-              >
-                <Flex>
-                  <Avatar size="lg" mr={4} src={element.profilePic?`data:image/jpeg;base64,${element.profilePic}` : process.env.NEXT_PUBLIC_DEFAULT_PICTURE} />
-                  <Box>
-                    <Heading as="h2" size="md" mb={2}>
-                      {`${element.firstName} ${element.lastName}`}
-                      
-                    </Heading>
-                    <Text mb={2}>{element.text}</Text>
-                  </Box>
-                </Flex>
-                <Spacer />
-              </Flex>
-            ))
+      <div data-testid="inbox-page">
+        <Layout>
+          <NavBar></NavBar>
+          {loading == true ? (
+            <Spinner />
           ) : (
-            <h1>No new message</h1>
+            <Box p={50}>
+              <Heading as="h1" size="lg" mb={4}>
+                Inbox
+              </Heading>
+              {messages.length > 0 ? (
+                messages.map((element: any) => (
+                  <Flex
+                    key={element.id}
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    p={4}
+                    mb={4}
+                    display="flex"
+                    alignItems="center"
+                    cursor={'pointer'}
+                    onClick={() => router.push(`/inbox/${element.id}`)}
+                  >
+                    <Flex>
+                      <Avatar
+                        size="lg"
+                        mr={4}
+                        src={
+                          element.profilePic
+                            ? `data:image/jpeg;base64,${element.profilePic}`
+                            : process.env.NEXT_PUBLIC_DEFAULT_PICTURE
+                        }
+                      />
+                      <Box>
+                        <Heading as="h2" size="md" mb={2}>
+                          {`${element.firstName} ${element.lastName}`}
+                        </Heading>
+                        <Text mb={2}>{element.text}</Text>
+                      </Box>
+                    </Flex>
+                    <Spacer />
+                  </Flex>
+                ))
+              ) : (
+                <h1>No new message</h1>
+              )}
+            </Box>
           )}
-        </Box>
-          }
-        
-      </Layout>
+        </Layout>
+      </div>
     </>
   )
 }
