c436bb17690b3fdef25d1e8da4c7a0798b1a5855
Shadi <shadosite@gmail.com>
2023-03-22 15:38:37 -0400

    Now will upload files and save them on shado-cloud (temporary until we decode on a service)

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -4,13 +4,21 @@ import * as Pusher from 'pusher'
 import { Message } from '../models/message.entity'
 import { type User } from '../models/user.entity'
 import { Repository } from 'typeorm'
+import { ShadoCloudClient } from 'shado-cloud-sdk'
+import { createReadStream } from 'fs'
+import { UploadedFileDB } from '../models/file.entity'
 
 @Injectable()
 export class ChatService {
   private readonly pusher: Pusher
+  private readonly cloud: ShadoCloudClient
+
   constructor (
     @InjectRepository(Message)
-    private readonly messageRepository: Repository<Message>) {
+    private readonly messageRepository: Repository<Message>,
+    @InjectRepository(UploadedFileDB)
+    private readonly fileRepository: Repository<UploadedFileDB>
+  ) {
     // TODO: These should be in a .env file
     this.pusher = new Pusher({
       appId: process.env.PUSHER_APP_ID ?? 'unset',
@@ -20,10 +28,19 @@ export class ChatService {
       useTLS: true
       // encrypted: true
     })
+
+    this.cloud = new ShadoCloudClient('shadi@shado.com', 'shadi1234')
   }
 
-  public async message (sender: User, receiver: User, message: string): Promise<Pusher.Response> {
-    const res = await this.pusher.trigger(`message-${receiver.id}`, 'message', { message, sender: sender.id })
+  public async message (
+    sender: User,
+    receiver: User,
+    message: string
+  ): Promise<Pusher.Response> {
+    const res = await this.pusher.trigger(`message-${receiver.id}`, 'message', {
+      message,
+      sender: sender.id
+    })
     // Push to db (we don't need to await --> save time)
     const msg = new Message()
     msg.message = message
@@ -49,9 +66,51 @@ export class ChatService {
     return Array.from(conversations)
   }
 
-  public async conversation (id: number, withUserId: number): Promise<Message[]> {
+  public async conversation (
+    id: number,
+    withUserId: number
+  ): Promise<Message[]> {
     return await this.messageRepository.find({
-      where: [{ senderId: id, receiverId: withUserId }, { senderId: withUserId, receiverId: id }]
+      where: [
+        { senderId: id, receiverId: withUserId },
+        { senderId: withUserId, receiverId: id }
+      ]
     })
+  }
+
+  /**
+	 * TODO: move this to a separate service
+	 * @param file
+	 * @param userId
+	 */
+  public async upload (
+    file: Express.Multer.File,
+    userId: number
+  ): Promise<void> {
+    const storedName = this.randomAlphaNumeric(30)
+    const res = await this.cloud.file.upload({
+      file: createReadStream(file.buffer),
+      dest: '',
+      name: storedName
+    })
+    console.log(res)
+    const dbFileData = new UploadedFileDB()
+    dbFileData.originalName = file.originalname
+    dbFileData.storedName = storedName
+    dbFileData.mime = file.mimetype
+    dbFileData.userId = userId
+    dbFileData.size = file.size
+    await this.fileRepository.save(dbFileData)
+  }
+
+  private randomAlphaNumeric (length: number): string {
+    let result = ''
+    const characters =
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
   }
 }
---
+++ backend/src/models/file.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+
+@Entity()
+export class UploadedFileDB {
+  @PrimaryGeneratedColumn()
+    id: number
+
+  @Column()
+    originalName: string
+
+  @Column()
+    storedName: string
+
+  @Column()
+    mime: string
+
+  @Column()
+    userId: number
+
+  @Column()
+    size: number
+
+  @CreateDateColumn()
+    createdAt: Date
+}
