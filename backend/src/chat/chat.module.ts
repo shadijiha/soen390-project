6877410b3ac94ce1c39e23124036d5125116c3bd
Shadi <shadosite@gmail.com>
2023-03-22 15:39:52 -0400

    Update chat.module.ts

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { Job } from '../models/job.entity'
-import { User } from '../models/user.entity'
-import { UsersService } from '../users/users.service'
-import { Message } from '../models/message.entity'
-import { ChatController } from './chat.controller'
-import { ChatService } from './chat.service'
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Job } from "../models/job.entity";
+import { User } from "../models/user.entity";
+import { UsersService } from "../users/users.service";
+import { Message } from "../models/message.entity";
+import { ChatController } from "./chat.controller";
+import { ChatService } from "./chat.service";
+import { UploadedFileDB } from "../models/file.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message, User, Job])],
-  controllers: [ChatController],
-  providers: [ChatService, UsersService]
+	imports: [TypeOrmModule.forFeature([Message, User, Job, UploadedFileDB])],
+	controllers: [ChatController],
+	providers: [ChatService, UsersService],
 })
 export class ChatModule {}
