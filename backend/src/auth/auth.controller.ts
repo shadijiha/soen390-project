b1a1d79f094ed2d05b0588765c3269712da6483f
Marc-Eid <marc.mn.eid@gmail.com>
2023-03-23 20:54:22 -0400

    comments for auth component

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,67 +1,70 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
-import { ConflictException } from '@nestjs/common/exceptions'
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { ConnectionsService } from '../users/connections/connections.service'
-import { User } from '../models/user.entity'
-import { UsersService } from '../users/users.service'
-import { AuthUser, BearerPayload } from '../util/util'
-import { AuthService } from './auth.service'
-import { Auth } from './auth.types'
-import { JwtAuthGuard } from './jwt-auth.guard'
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { ConflictException } from "@nestjs/common/exceptions";
+import { ApiBearerAuth, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ConnectionsService } from "../users/connections/connections.service";
+import { User } from "../models/user.entity";
+import { UsersService } from "../users/users.service";
+import { AuthUser, BearerPayload } from "../util/util";
+import { AuthService } from "./auth.service";
+import { Auth } from "./auth.types";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 
-@Controller('auth')
-@ApiTags('Authentication')
+@Controller("auth")
+@ApiTags("Authentication")
 export class AuthController {
-  constructor (
+  constructor(
     private readonly userService: UsersService,
     private readonly authService: AuthService,
     private readonly connectionsService: ConnectionsService
   ) {}
 
-  @Post('login')
+  //login endpoint for user
+  @Post("login")
   @ApiResponse({ type: Auth.LoginResponse })
-  public async login (@Body() body: Auth.LoginRequest): Promise<Auth.LoginResponse> {
+  public async login(@Body() body: Auth.LoginRequest): Promise<Auth.LoginResponse> {
     // try {
-    return await this.authService.login(body)
+    return await this.authService.login(body);
     // } catch (e) {
     // 	return error<Auth.LoginResponse>(e);
     // }
   }
 
-  @Post('register')
+  //register and then login endpoint for user
+  @Post("register")
   @ApiResponse({ type: Auth.LoginResponse })
-  public async register (@Body() body: Auth.RegisterRequest): Promise<Auth.LoginResponse> {
+  public async register(@Body() body: Auth.RegisterRequest): Promise<Auth.LoginResponse> {
     try {
       // Will fail if email is NOT taken
-      await this.userService.findOneByEmail(body.email)
+      await this.userService.findOneByEmail(body.email);
     } catch (e) {
-      await this.userService.create(body)
-      return await this.authService.login(body)
+      await this.userService.create(body);
+      return await this.authService.login(body);
       // from security point we should not tell to a user that this email was taken already. Message should tell there was an error but not that this email is alrady in the database. Kinda aan easy wau for the hackers.
     }
 
     // If we are here this means that email is taken since we did not return from the try block
-    throw new ConflictException(`Email ${body.email} already taken`)
+    throw new ConflictException(`Email ${body.email} already taken`);
   }
 
-  @Get('me')
+  //get user info endpoint
+  @Get("me")
   @ApiResponse({ type: User })
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  public async me (@AuthUser() authedUser: BearerPayload): Promise<User> {
+  public async me(@AuthUser() authedUser: BearerPayload): Promise<User> {
     const user: User = (await authedUser.getUser([
-      'educations',
-      'workExperiences',
-      'volunteeringExperience',
-      'skills',
-      'recommendationsReceived',
-      'recommendationsGiven',
-      'courses',
-      'projects',
-      'awards',
-      'languages'
-    ])) as User
+      "educations",
+      "workExperiences",
+      "volunteeringExperience",
+      "skills",
+      "recommendationsReceived",
+      "recommendationsGiven",
+      "courses",
+      "projects",
+      "awards",
+      "languages",
+    ])) as User;
 
-    return user
+    return user;
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,60 +1,62 @@
-import { Injectable } from '@nestjs/common'
-import { UnauthorizedException } from '@nestjs/common/exceptions'
-import { JwtService } from '@nestjs/jwt'
-import { InjectRepository } from '@nestjs/typeorm'
-import * as argon2 from 'argon2'
-import { Repository } from 'typeorm'
-import { User } from '../models/user.entity'
-import { type Auth } from './auth.types'
+import { Injectable } from "@nestjs/common";
+import { UnauthorizedException } from "@nestjs/common/exceptions";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import * as argon2 from "argon2";
+import { Repository } from "typeorm";
+import { User } from "../models/user.entity";
+import { type Auth } from "./auth.types";
 
 @Injectable()
 export class AuthService {
-  constructor (
+  constructor(
     private readonly jwtService: JwtService,
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>
   ) {}
 
-  public async validateUser (email: string, pass: string): Promise<Partial<User> | null> {
+  //validates user email vs password
+  public async validateUser(email: string, pass: string): Promise<Partial<User> | null> {
     const user = await this.usersRepository
-      .createQueryBuilder('user')
-      .select('user.password')
-      .addSelect('user')
-      .where('email = :email', {
-        email
+      .createQueryBuilder("user")
+      .select("user.password")
+      .addSelect("user")
+      .where("email = :email", {
+        email,
       })
-      .getOne()
+      .getOne();
 
     // TODO: implement Argon encryption
     if (user != null && (await argon2.verify(user.password, pass))) {
-      const { password, ...result } = user // Remove password from user object (we don't want to return it)
-      return result
+      const { password, ...result } = user; // Remove password from user object (we don't want to return it)
+      return result;
     }
-    return null
+    return null;
   }
 
-  public async login ({ email, password }: Auth.LoginRequest): Promise<{ user: User, access_token: string }> {
+  //logins user
+  public async login({ email, password }: Auth.LoginRequest): Promise<{ user: User; access_token: string }> {
     // Validate email
-    let user: User
+    let user: User;
     try {
-      user = await this.usersRepository.findOneByOrFail({ email })
+      user = await this.usersRepository.findOneByOrFail({ email });
     } catch (e) {
-      throw new UnauthorizedException(`Email ${email} is invalid`)
+      throw new UnauthorizedException(`Email ${email} is invalid`);
     }
 
     // validate password
     if ((await this.validateUser(email, password)) != null) {
       const payload = {
         email,
-        id: user.id
-      }
+        id: user.id,
+      };
 
       return {
         user,
-        access_token: this.jwtService.sign(payload)
-      }
+        access_token: this.jwtService.sign(payload),
+      };
     } else {
-      throw new UnauthorizedException('Wrong password for ' + email)
+      throw new UnauthorizedException("Wrong password for " + email);
     }
   }
 }
