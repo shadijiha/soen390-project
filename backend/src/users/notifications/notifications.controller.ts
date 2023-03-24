0e8f0ecf75feb26defec1b0104d4acfc63b2f02b
Marc-Eid <marc.mn.eid@gmail.com>
2023-03-23 20:22:28 -0400

    comment documentaiton for the notification componenet

--- backend/src/users/notifications/notifications.controller.ts
+++ backend/src/users/notifications/notifications.controller.ts
@@ -1,25 +1,26 @@
-import { Controller } from '@nestjs/common/decorators/core/controller.decorator'
-import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator'
-import { Post } from '@nestjs/common/decorators/http/request-mapping.decorator'
-import { ApiBearerAuth } from '@nestjs/swagger/dist/decorators/api-bearer.decorator'
-import { ApiTags } from '@nestjs/swagger/dist/decorators/api-use-tags.decorator'
-import { PusherService } from '../../util/pusher/pusher.service'
-import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
-import { AuthUser, BearerPayload } from '../../util/util'
+import { Controller } from "@nestjs/common/decorators/core/controller.decorator";
+import { UseGuards } from "@nestjs/common/decorators/core/use-guards.decorator";
+import { Post } from "@nestjs/common/decorators/http/request-mapping.decorator";
+import { ApiBearerAuth } from "@nestjs/swagger/dist/decorators/api-bearer.decorator";
+import { ApiTags } from "@nestjs/swagger/dist/decorators/api-use-tags.decorator";
+import { PusherService } from "../../util/pusher/pusher.service";
+import { JwtAuthGuard } from "../../auth/jwt-auth.guard";
+import { AuthUser, BearerPayload } from "../../util/util";
 
-@Controller('notifications')
-@ApiTags('Notifications')
+@Controller("notifications")
+@ApiTags("Notifications")
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 export class NotificationsController {
-  constructor (private readonly pusherService: PusherService) {}
+  constructor(private readonly pusherService: PusherService) {}
 
-  @Post('/notifications')
-  async sendNotification (
-    @AuthUser() userInfo: BearerPayload
-  ): Promise<void> {
-    await this.pusherService.triggerNotification('my-channel', 'my-event', {
-      message: 'Hello, world!'
-    })
+  /**
+   * It sends a notification to the client
+   */
+  @Post("/notifications")
+  async sendNotification(@AuthUser() userInfo: BearerPayload): Promise<void> {
+    await this.pusherService.triggerNotification("my-channel", "my-event", {
+      message: "Hello, world!",
+    });
   }
 }
