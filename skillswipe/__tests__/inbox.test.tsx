b945cc4628eda1c6629f80042eaff5c57f4f3c16
Uzair Ali <uzairali245@gmail.com>
2023-03-24 16:22:38 -0400

    render test of inbox page

---
+++ skillswipe/__tests__/inbox.test.tsx
@@ -0,0 +1,29 @@
+import Layout from '@/components/Layout'
+import Inbox from '@/pages/inbox/index'
+import '@testing-library/jest-dom'
+import { render, screen, waitFor } from '@testing-library/react'
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from '../src/Redux/store'
+
+jest.mock('next/router', () => require('next-router-mock'))
+describe('Inbox', () => {
+  const renderInbox = () =>
+    render(
+      <Provider store={store}>
+        {' '}
+        <Layout>
+          <Inbox />
+        </Layout>
+      </Provider>
+    )
+  //   beforeAll(()=>{
+
+  // })
+  it('should display Inbox page without crashing', async () => {
+    renderInbox()
+    await waitFor(() => {
+      expect(screen.getByTestId('inbox')).toBeInTheDocument()
+    })
+  })
+})
