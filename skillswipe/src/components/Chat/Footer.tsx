b6802b320b45b85efbb3046e93919a0d9094f896
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-03-25 02:42:45 -0400

    UI fixed

--- skillswipe/src/components/Chat/Footer.tsx
+++ skillswipe/src/components/Chat/Footer.tsx
@@ -1,36 +1,56 @@
+import { AttachmentIcon } from '@chakra-ui/icons'
 import { Button, Flex, Input, useColorMode } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { IoSendSharp } from 'react-icons/io5'
 
 const Footer = ({ inputMessage, setInputMessage, handleSendMessage }) => {
-	const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode()
+  const input = useRef(document.createElement("div"));
+  const handleClick = () =>{
+		input.current.click();
+  }
+  
   return (
-
-	<Flex w="100%" mt="5">
-  	<Input
+	<>
+    <Flex w="100%" mt="5">
+      <Input
         borderRadius="10px"
-        color={colorMode === 'light' ? "black" : "white"}
-    	placeholder="Type Something..."
-		
-    	
-    	onKeyPress={(e) => {
-          if (e.key === 'Enter') {
+        color={colorMode === 'light' ? 'black' : 'white'}
+        placeholder="Type Something..."
+        onKeyPress={(e) => {
+			if (e.key === 'Enter') {
             handleSendMessage()
-			}
-    	}}
-    	value={inputMessage}
-    	onChange={(e) => setInputMessage(e.target.value)}
-		/>
-	
-  	<Button
+		}
+	}}
+	value={inputMessage}
+	onChange={(e) => setInputMessage(e.target.value)}
+	/>
+      <Button
         borderRadius="10px"
-    	disabled={inputMessage.trim().length <= 0}
-    	onClick={handleSendMessage}
-        ml={'2rem'}
-  	>
-    	Send
-  	</Button>
-	</Flex>
+        disabled={inputMessage.trim().length <= 0}
+        ml={'1rem'}
+		
+		onClick={handleClick}
+      >
+	
+
+          <AttachmentIcon />
+
+      </Button>
+
+      <Button
+        borderRadius="10px"
+        disabled={inputMessage.trim().length <= 0}
+        onClick={handleSendMessage}
+        ml={'1rem'}
+		>
+        Send
+      </Button>
+    </Flex>
+	<input ref={input} type="file" style={{display : 'none'}}>
+	</input>
+	</>
+	
   )
 }
 
