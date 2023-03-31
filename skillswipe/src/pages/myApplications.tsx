f91016ddf9aa9e05a0ebd3a48efc64719c1078b0
Usama Saleem <usama.saleem@hotmail.com>
2023-03-31 00:20:48 -0400

    Update myApplications.tsx
    Added conditional rendering to show if no jobs were applied to yet and a hyperlink to go to that job

--- skillswipe/src/pages/myApplications.tsx
+++ skillswipe/src/pages/myApplications.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 import Layout from '@/components/Layout'
@@ -204,107 +206,143 @@ const MyApplications = () => {
               </Menu> */}
             </HStack>
           </Flex>
-          <VStack
-            shadow={{ base: 'none', md: 'md' }}
-            border="1px solid"
-            borderColor="gray.400"
-            rounded="15px"
-            overflow="hidden"
-            spacing={0}
-            marginBottom={'5em'}
-          >
-            {applications.map((application, index) => (
-              <Fragment key={index}>
-                <Grid
-                  templateRows={{ base: 'auto auto', md: 'auto' }}
-                  w="100%"
-                  templateColumns={{ base: 'unset', md: '4fr 3fr 2fr' }}
-                  p={{ base: 2, sm: 4 }}
-                  gap={3}
-                  alignItems="center"
-                  _hover={{ bg: useColorModeValue('gray.200', 'gray.700') }}
-                >
-                  <Box gridColumnEnd={{ base: 'span 2', md: 'unset' }}>
-                    <HStack spacing={3}>
-                      <img
-                        src={`http://www.${application.job.companyName.toLowerCase()}.com/favicon.ico`}
-                        width="20px"
-                        height="20px"
-                        alt="logo"
-                        onError={(e) => {
-                          // show a default image if the company logo is not found
-                          e.currentTarget.src =
-                            'https://img.icons8.com/3d-fluency/512/hard-working.png'
-                        }}
-                      />
-
-                      <chakra.h2 fontWeight="bold" fontSize="lg">
-                        {application.job.companyName}
-                      </chakra.h2>
-                    </HStack>
-
-                    <chakra.h3
-                      as={Link}
-                      isExternal
-                      fontWeight="extrabold"
-                      fontSize="2xl"
-                      onClick={() => {
-                        router.push(`/jobListing/${application.job.id}`)
-                      }}
-                    >
-                      {application.job.jobTitle}
-                    </chakra.h3>
-                    <div
-                      style={{
-                        paddingTop: '0.5em',
-                      }}
-                    ></div>
-
-                    <chakra.p
-                      fontWeight="bold"
-                      fontSize="sm"
-                      color={useColorModeValue('gray.600', 'gray.300')}
-                    >
-                      📍 {application.job.location}
-                    </chakra.p>
-                    <chakra.p
-                      fontWeight="normal"
-                      fontSize="sm"
-                      color={useColorModeValue('gray.600', 'gray.300')}
-                    >
-                      💼 ‎
-                      {application.job.jobType.charAt(0).toUpperCase() +
-                        application.job.jobType.slice(1)}
-                    </chakra.p>
-                  </Box>
-                  <VStack
-                    spacing={{ base: 0, sm: 3 }}
-                    alignItems="start"
-                    fontWeight="light"
-                    fontSize={{ base: 'xs', sm: 'sm' }}
-                    color={useColorModeValue('gray.600', 'gray.300')}
-                  >
-                    {/* By the way, the ‎ is an invisible space character */}
-                    <chakra.p>
-                      {/* format the starting date to be only year month and date */}
-                      📅 ‎ ‎ Starting Date: {application.job.startDate.split('T')[0]}
-                    </chakra.p>
-                    <chakra.p>🤑 ‎ ‎ Salary: ${application.job.salary}/hr</chakra.p>
-                    <chakra.p>
-                      💼 ‎ ‎ Job Posted: {application.job.created_at.split('T')[0]}
-                    </chakra.p>
-                    <chakra.p>
-                      ⏳ ‎ ‎ Job Updated: {application.job.updated_at.split('T')[0]}
-                    </chakra.p>
-                  </VStack>
-                  <Stack
-                    spacing={6}
-                    direction="row"
-                    fontSize={{ base: 'sm', sm: 'md' }}
-                    justifySelf="flex-end"
+          {applications.length === 0 ? (
+            <Box
+              textAlign="center"
+              paddingTop={'2em'}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                alt="No Jobs"
+                src="https://img.icons8.com/3d-fluency/512/link.png"
+                style={{
+                  height: '60px',
+                  width: '60px',
+                  objectFit: 'cover',
+                  marginBottom: '2em',
+                }}
+              ></img>
+              <chakra.p fontSize="xl">
+                {/* click hyperlink to create application */}
+                You have not applied for any jobs yet.
+                <br />
+                <Link href="/openJobs" color={'blue.100'}>
+                  Click here
+                </Link>{' '}
+                to view available jobs.
+              </chakra.p>
+            </Box>
+          ) : (
+            <VStack
+              shadow={{ base: 'none', md: 'md' }}
+              border="1px solid"
+              borderColor="gray.400"
+              rounded="15px"
+              overflow="hidden"
+              spacing={0}
+              marginBottom={'5em'}
+            >
+              {applications.map((application, index) => (
+                <Fragment key={index}>
+                  <Grid
+                    templateRows={{ base: 'auto auto', md: 'auto' }}
+                    w="100%"
+                    templateColumns={{ base: 'unset', md: '4fr 3fr 2fr' }}
+                    p={{ base: 2, sm: 4 }}
+                    gap={3}
                     alignItems="center"
+                    _hover={{ bg: useColorModeValue('gray.200', 'gray.700') }}
                   >
-                    {/* <Button
+                    <Box gridColumnEnd={{ base: 'span 2', md: 'unset' }}>
+                      <HStack spacing={3}>
+                        <img
+                          src={`http://www.${application.job.companyName.toLowerCase()}.com/favicon.ico`}
+                          width="20px"
+                          height="20px"
+                          alt="logo"
+                          onError={(e) => {
+                            // show a default image if the company logo is not found
+                            e.currentTarget.src =
+                              'https://img.icons8.com/3d-fluency/512/hard-working.png'
+                          }}
+                        />
+
+                        <chakra.h2 fontWeight="bold" fontSize="lg">
+                          {application.job.companyName}
+                        </chakra.h2>
+                      </HStack>
+
+                      <chakra.h3
+                        as={Link}
+                        isExternal
+                        fontWeight="extrabold"
+                        fontSize="2xl"
+                        onClick={() => {
+                          router.push(`/jobListing/${application.job.id}`)
+                        }}
+                      >
+                        {application.job.jobTitle}
+                      </chakra.h3>
+                      <div
+                        style={{
+                          paddingTop: '0.5em',
+                        }}
+                      ></div>
+
+                      <chakra.p
+                        fontWeight="bold"
+                        fontSize="sm"
+                        color={useColorModeValue('gray.600', 'gray.300')}
+                      >
+                        📍 {application.job.location}
+                      </chakra.p>
+                      <chakra.p
+                        fontWeight="normal"
+                        fontSize="sm"
+                        color={useColorModeValue('gray.600', 'gray.300')}
+                      >
+                        💼 ‎
+                        {application.job.jobType.charAt(0).toUpperCase() +
+                          application.job.jobType.slice(1)}
+                      </chakra.p>
+                    </Box>
+                    <VStack
+                      spacing={{ base: 0, sm: 3 }}
+                      alignItems="start"
+                      fontWeight="light"
+                      fontSize={{ base: 'xs', sm: 'sm' }}
+                      color={useColorModeValue('gray.600', 'gray.300')}
+                    >
+                      {/* By the way, the ‎ is an invisible space character */}
+                      <chakra.p>
+                        {/* format the starting date to be only year month and date */}
+                        📅 ‎ ‎ Starting Date:{' '}
+                        {application.job.startDate.split('T')[0]}
+                      </chakra.p>
+                      <chakra.p>
+                        🤑 ‎ ‎ Salary: ${application.job.salary}/hr
+                      </chakra.p>
+                      <chakra.p>
+                        💼 ‎ ‎ Job Posted: {application.job.created_at.split('T')[0]}
+                      </chakra.p>
+                      <chakra.p>
+                        ⏳ ‎ ‎ Job Updated:{' '}
+                        {application.job.updated_at.split('T')[0]}
+                      </chakra.p>
+                    </VStack>
+                    <Stack
+                      spacing={6}
+                      direction="row"
+                      fontSize={{ base: 'sm', sm: 'md' }}
+                      justifySelf="flex-end"
+                      alignItems="center"
+                    >
+                      {/* <Button
                       as={Link}
                       _hover={{ bg: useColorModeValue('gray.400', 'gray.600') }}
                       p={5}
@@ -319,36 +357,37 @@ const MyApplications = () => {
                       Edit Listing
                     </Button> */}
 
-                    <Button
-                      as={Link}
-                      _hover={{ bg: useColorModeValue('gray.400', 'gray.600') }}
-                      p={5}
-                      colorScheme="red"
-                      rounded="100px"
-                      outline={'solid 1px'}
-                      outlineColor={useColorModeValue('gray.400', 'gray.600')}
-                      onClick={async () => {
-                        const token = localStorage.getItem('jwt')
-                        try {
-                          await withdrawJobApplication(token, application.id)
-                          setApplications(
-                            applications.filter((a) => a.id !== application.id)
-                          )
-                          toast.success('Application withdrawn successfully!')
-                        } catch (error) {
-                          console.error(error)
-                          toast.error('Error withdrawing application')
-                        }
-                      }}
-                    >
-                      Withdraw Application
-                    </Button>
-                  </Stack>
-                </Grid>
-                {jobListing.length - 1 !== index && <Divider m={0} />}
-              </Fragment>
-            ))}
-          </VStack>
+                      <Button
+                        as={Link}
+                        _hover={{ bg: useColorModeValue('gray.400', 'gray.600') }}
+                        p={5}
+                        colorScheme="red"
+                        rounded="100px"
+                        outline={'solid 1px'}
+                        outlineColor={useColorModeValue('gray.400', 'gray.600')}
+                        onClick={async () => {
+                          const token = localStorage.getItem('jwt')
+                          try {
+                            await withdrawJobApplication(token, application.id)
+                            setApplications(
+                              applications.filter((a) => a.id !== application.id)
+                            )
+                            toast.success('Application withdrawn successfully!')
+                          } catch (error) {
+                            console.error(error)
+                            toast.error('Error withdrawing application')
+                          }
+                        }}
+                      >
+                        Withdraw Application
+                      </Button>
+                    </Stack>
+                  </Grid>
+                  {jobListing.length - 1 !== index && <Divider m={0} />}
+                </Fragment>
+              ))}
+            </VStack>
+          )}
         </Container>
       </Layout>
     </>
