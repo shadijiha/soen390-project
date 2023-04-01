53d3295a0ba86948490d28c6beafade289315a19
Usama Saleem <usama.saleem@hotmail.com>
2023-03-31 21:37:57 -0400

    Update next.config.js

--- skillswipe/next.config.js
+++ skillswipe/next.config.js
@@ -1,25 +1,23 @@
-// const { i18n } = require('./next-i18next.config')
-// // /** @type {import('./next-i18next.config').i18n} */
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig = {
-//   reactStrictMode: false,
-//   eslint: {
-//     // Warning: This allows production builds to successfully complete even if
-//     // your project has ESLint errors.
-//     ignoreDuringBuilds: true,
-//   },
-
-//   env: {
-//     BASE_URL: process.env.BASE_URL,
-//   },
-//   i18n,
-// }
-
-// module.exports = nextConfig
-
-//----------------------------------------------------------------------
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { i18n } = require('./next-i18next.config')
+/** @type {import('./next-i18next.config').i18n} */
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  i18n,
+}
+
+module.exports = nextConfig
 
 module.exports = {
   i18n,
