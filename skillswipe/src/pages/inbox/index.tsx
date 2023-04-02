614d73347e97fe70c069ef48ecb909ffc032a727
Usama Saleem <usama.saleem@hotmail.com>
2023-04-01 22:41:45 -0400

    Update index.tsx

--- skillswipe/src/pages/inbox/index.tsx
+++ skillswipe/src/pages/inbox/index.tsx
@@ -1,11 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable @next/next/no-img-element */
 import Layout from '@/components/Layout'
 import NavBar from '@/components/NavBar'
-import {
-  Avatar, Box, Flex,
-  Heading, Spacer, Text
-} from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, Spacer, Text } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
@@ -13,82 +11,76 @@ import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import { getAllConversation } from '../api/chat'
-  const router = useRouter()
-  const [messages, setMessages] = useState([{}])
-  const [loading, setLoading] = useState(true)
-  const User = useSelector((state) => state as any)
-  useEffect(() => {
-    if (User.auth) {
-      const token = localStorage.getItem('jwt')
-      getAllConversation(token)
-        .then((response) => {
-          let allConvo = response.data
-          allConvo = allConvo.filter(filterConvo)
-          setMessages(allConvo)
-          setLoading(false)
-        })
-        .catch((error) => {
-          toast(error.message)
-        })
-    }
-  }, [User.auth])
-  const filterConvo = (element: any) => {
-    return element.id != User.auth.id
+const router = useRouter()
+const [messages, setMessages] = useState([{}])
+const [loading, setLoading] = useState(true)
+const User = useSelector((state) => state as any)
+useEffect(() => {
+  if (User.auth) {
+    const token = localStorage.getItem('jwt')
+    getAllConversation(token)
+      .then((response) => {
+        let allConvo = response.data
+        allConvo = allConvo.filter(filterConvo)
+        setMessages(allConvo)
+        setLoading(false)
+      })
+      .catch((error) => {
+        toast(error.message)
+      })
   }
-  return (
-    <>
-      <Layout>
-        <NavBar></NavBar>
-
-        <Box p={50} data-testid="inbox">
-          <Heading as="h1" size="lg" mb={4}>
-            {t('inbox')}
-          </Heading>
-          {messages.length > 0 ? (
-            messages.map((element: any) => (
-              <Flex
-                key={element.id}
-                borderWidth="1px"
-                borderRadius="lg"
-                p={4}
-                mb={4}
-                display="flex"
-                alignItems="center"
-                cursor={'pointer'}
-                onClick={() => router.push(`/inbox/${element.id}`)}
-              >
-                <Flex>
-                  <Avatar
-                    size="lg"
-                    mr={4}
-                    src={
-                      element.profilePic
-                        ? `data:image/jpeg;base64,${element.profilePic}`
-                        : process.env.NEXT_PUBLIC_DEFAULT_PICTURE
-                    }
-                  />
-                  <Box>
-                    <Heading as="h2" size="md" mb={2}>
-                      {`${element.firstName} ${element.lastName}`}
-                    </Heading>
-                    <Text mb={2}>{element.text}</Text>
-                  </Box>
-                </Flex>
-                <Spacer />
-              </Flex>
-            ))
-          ) : (
-            <h1>{t('noMessages')}</h1>
-          )}
-        </Box>
-      </Layout>
-    </>
-  )
+}, [User.auth])
+const filterConvo = (element: any) => {
+  return element.id != User.auth.id
 }
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common'])),
-  },
-})
+return (
+  <>
+    <Layout>
+      <NavBar></NavBar>
+
+      <Box p={50} data-testid="inbox">
+        <Heading as="h1" size="lg" mb={4}>
+          {t('inbox')}
+        </Heading>
+        {messages.length > 0 ? (
+          messages.map((element: any) => (
+            <Flex
+              key={element.id}
+              borderWidth="1px"
+              borderRadius="lg"
+              p={4}
+              mb={4}
+              display="flex"
+              alignItems="center"
+              cursor={'pointer'}
+              onClick={() => router.push(`/inbox/${element.id}`)}
+            >
+              <Flex>
+                <Avatar
+                  size="lg"
+                  mr={4}
+                  src={
+                    element.profilePic
+                      ? `data:image/jpeg;base64,${element.profilePic}`
+                      : process.env.NEXT_PUBLIC_DEFAULT_PICTURE
+                  }
+                />
+                <Box>
+                  <Heading as="h2" size="md" mb={2}>
+                    {`${element.firstName} ${element.lastName}`}
+                  </Heading>
+                  <Text mb={2}>{element.text}</Text>
+                </Box>
+              </Flex>
+              <Spacer />
+            </Flex>
+          ))
+        ) : (
+          <h1>{t('noMessages')}</h1>
+        )}
+      </Box>
+    </Layout>
+  </>
+)
 
 export default Inbox
