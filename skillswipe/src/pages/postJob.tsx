dff6d534f1430d88bd6b6c6a7f90253d66434014
Usama Saleem <usama.saleem@hotmail.com>
2023-04-05 23:15:57 -0400

    lint
    lint fixes

--- skillswipe/src/pages/postJob.tsx
+++ skillswipe/src/pages/postJob.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/jsx-no-undef */
 import NavBar from '@/components/NavBar'
