7a1d5af1e9298bfeb9cff63cd7adaf054406ad56
Usama Saleem <usama.saleem@hotmail.com>
2023-04-07 16:51:13 -0400

    Update home.tsx

--- skillswipe/src/pages/home.tsx
+++ skillswipe/src/pages/home.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/alt-text */
