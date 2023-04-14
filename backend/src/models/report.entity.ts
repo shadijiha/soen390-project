4d11e2e306b278096816039d6b19e5a6da391acb
Shadi <shadosite@gmail.com>
2023-04-14 17:13:55 -0400

    lint

--- backend/src/models/report.entity.ts
+++ backend/src/models/report.entity.ts
@@ -1,29 +1,29 @@
 import {
-	BaseEntity,
-	Column,
-	CreateDateColumn,
-	Entity,
-	PrimaryGeneratedColumn,
-	UpdateDateColumn,
-} from "typeorm";
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm'
 
 @Entity()
 export class Report extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+    id: number
 
-	@Column()
-	reported_by_id: number;
+  @Column()
+    reported_by_id: number
 
-	@Column()
-	type: "post" | "chat";
+  @Column()
+    type: 'post' | 'chat'
 
-	@Column()
-	entity_id: number;
+  @Column()
+    entity_id: number
 
-	@CreateDateColumn()
-	created_at: Date;
+  @CreateDateColumn()
+    created_at: Date
 
-	@UpdateDateColumn()
-	updated_at: Date;
+  @UpdateDateColumn()
+    updated_at: Date
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,197 +1,197 @@
-import { Injectable } from "@nestjs/common";
-import { User } from "../models/user.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { DataSource, Like, Repository } from "typeorm";
-import { type Users } from "./users.types";
-import * as argon2 from "argon2";
-import { type Auth } from "../auth/auth.types";
-import { Job } from "../models/job.entity";
-import type * as Pusher from "pusher";
-import { PusherService } from "../util/pusher/pusher.service";
-import { Report } from "../models/report.entity";
+import { Injectable } from '@nestjs/common'
+import { User } from '../models/user.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { DataSource, Like, Repository } from 'typeorm'
+import { type Users } from './users.types'
+import * as argon2 from 'argon2'
+import { type Auth } from '../auth/auth.types'
+import { Job } from '../models/job.entity'
+import type * as Pusher from 'pusher'
+import { PusherService } from '../util/pusher/pusher.service'
+import { Report } from '../models/report.entity'
 
 @Injectable()
 export class UsersService {
-	constructor(
-		@InjectRepository(User)
-		private readonly usersRepository: Repository<User>,
-		@InjectRepository(Job)
-		private readonly jobsRepository: Repository<Job>,
-		private readonly dataSource: DataSource,
-		private readonly pusherService: PusherService
-	) {}
+  constructor (
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+    @InjectRepository(Job)
+    private readonly jobsRepository: Repository<Job>,
+    private readonly dataSource: DataSource,
+    private readonly pusherService: PusherService
+  ) {}
 
-	/**
+  /**
 	 * It returns a user from the database by email
 	 * @param {string} email - string
 	 * @returns The user object
 	 */
-	public async getByEmail(email: string): Promise<User> {
-		return await this.usersRepository.findOneByOrFail({ email });
-	}
+  public async getByEmail (email: string): Promise<User> {
+    return await this.usersRepository.findOneByOrFail({ email })
+  }
 
-	/**
+  /**
 	 * It returns an array of users.
 	 * @returns An array of users
 	 */
-	async findAll(): Promise<User[]> {
-		return await this.usersRepository.find();
-	}
+  async findAll (): Promise<User[]> {
+    return await this.usersRepository.find()
+  }
 
-	/**
+  /**
 	 * It finds a user by their id and returns the user with all of their relations.
 	 * @param {number} userId - number - the id of the user we want to find
 	 * @param {string[]} [relations] - [
 	 * @returns The user object with all the relations.
 	 */
-	async findOneById(userId: number, relations?: string[]): Promise<User> {
-		const user: User = await this.usersRepository.findOneOrFail({
-			where: {
-				id: userId,
-			},
-			relations: [
-				"educations",
-				"workExperiences",
-				"volunteeringExperience",
-				"skills",
-				"courses",
-				"projects",
-				"awards",
-				"languages",
-				"recommendationsReceived",
-			],
-		});
+  async findOneById (userId: number, relations?: string[]): Promise<User> {
+    const user: User = await this.usersRepository.findOneOrFail({
+      where: {
+        id: userId
+      },
+      relations: [
+        'educations',
+        'workExperiences',
+        'volunteeringExperience',
+        'skills',
+        'courses',
+        'projects',
+        'awards',
+        'languages',
+        'recommendationsReceived'
+      ]
+    })
 
-		return user;
-	}
+    return user
+  }
 
-	/**
+  /**
 	 * "Find a user by their id, and return the user if found, otherwise throw an error."
 	 * @param {number} userId - number - the id of the user you want to find
 	 * @returns A user object with all the relations.
 	 */
-	async findOneByIdNoRelations(userId: number): Promise<User> {
-		const user: User = await this.usersRepository.findOneOrFail({
-			where: {
-				id: userId,
-			},
-		});
-		return user;
-	}
+  async findOneByIdNoRelations (userId: number): Promise<User> {
+    const user: User = await this.usersRepository.findOneOrFail({
+      where: {
+        id: userId
+      }
+    })
+    return user
+  }
 
-	/**
+  /**
 	 * Find a user by email, or throw an error if not found.
 	 * @param {string} email - string
 	 * @returns The user object
 	 */
-	async findOneByEmail(email: string): Promise<User | null> {
-		return await this.usersRepository.findOneByOrFail({ email });
-	}
+  async findOneByEmail (email: string): Promise<User | null> {
+    return await this.usersRepository.findOneByOrFail({ email })
+  }
 
-	/**
+  /**
 	 * It creates a new user, hashes the password, and saves the user to the database.
 	 * @param body - Auth.RegisterRequest
 	 * @returns The userNoPass is being returned.
 	 */
-	public async create(
-		body: Auth.RegisterRequest
-	): Promise<Record<string, any>> {
-		const user = new User();
-		user.email = body.email;
-		user.password = await argon2.hash(body.password);
-		user.firstName = body.firstName;
-		user.lastName = body.lastName;
-		user.gender = body.gender;
+  public async create (
+    body: Auth.RegisterRequest
+  ): Promise<Record<string, any>> {
+    const user = new User()
+    user.email = body.email
+    user.password = await argon2.hash(body.password)
+    user.firstName = body.firstName
+    user.lastName = body.lastName
+    user.gender = body.gender
 
-		const { password, ...userNoPass }: Record<string, any> =
-			await this.usersRepository.save(user);
-		return userNoPass;
-	}
+    const { password, ...userNoPass }: Record<string, any> =
+			await this.usersRepository.save(user)
+    return userNoPass
+  }
 
-	/**
+  /**
 	 * It updates a user object.
 	 * @param {number} id - number - the id of the user to be updated
 	 * @param user - Users.UpdateUserRequest
 	 * @param files - { profilePic?: Express.Multer.File; coverPic?: Express.Multer.File }
 	 * @returns The updated user.
 	 */
-	async update(
-		id: number,
-		user: Users.UpdateUserRequest,
-		files: { profilePic?: Express.Multer.File; coverPic?: Express.Multer.File }
-	): Promise<User> {
-		const oldUser = await this.findOneByIdNoRelations(id);
+  async update (
+    id: number,
+    user: Users.UpdateUserRequest,
+    files: { profilePic?: Express.Multer.File, coverPic?: Express.Multer.File }
+  ): Promise<User> {
+    const oldUser = await this.findOneByIdNoRelations(id)
 
-		oldUser.firstName =
-			user.firstName !== "" ? user.firstName : oldUser.firstName;
-		oldUser.lastName = user.lastName !== "" ? user.lastName : oldUser.lastName;
-		oldUser.email = user.email !== "" ? user.email : oldUser.email;
-		oldUser.mobileNo = user.mobileNo !== "" ? user.mobileNo : oldUser.mobileNo;
-		oldUser.gender = user.gender !== "" ? user.gender : oldUser.gender;
-		oldUser.biography =
-			user.biography !== "" ? user.biography : oldUser.biography;
+    oldUser.firstName =
+			user.firstName !== '' ? user.firstName : oldUser.firstName
+    oldUser.lastName = user.lastName !== '' ? user.lastName : oldUser.lastName
+    oldUser.email = user.email !== '' ? user.email : oldUser.email
+    oldUser.mobileNo = user.mobileNo !== '' ? user.mobileNo : oldUser.mobileNo
+    oldUser.gender = user.gender !== '' ? user.gender : oldUser.gender
+    oldUser.biography =
+			user.biography !== '' ? user.biography : oldUser.biography
 
-		// convert image to base64
-		if (files?.profilePic != null) {
-			const buff = files.profilePic[0].buffer;
-			const base64data = buff.toString("base64");
-			oldUser.profilePic = base64data;
-		}
+    // convert image to base64
+    if (files?.profilePic != null) {
+      const buff = files.profilePic[0].buffer
+      const base64data = buff.toString('base64')
+      oldUser.profilePic = base64data
+    }
 
-		// convert image to base64
-		if (files?.coverPic != null) {
-			const buff = files.coverPic[0].buffer;
-			const base64data = buff.toString("base64");
-			oldUser.coverPic = base64data;
-		}
+    // convert image to base64
+    if (files?.coverPic != null) {
+      const buff = files.coverPic[0].buffer
+      const base64data = buff.toString('base64')
+      oldUser.coverPic = base64data
+    }
 
-		await this.usersRepository.update(id, oldUser);
-		return await this.findOneById(id);
-	}
+    await this.usersRepository.update(id, oldUser)
+    return await this.findOneById(id)
+  }
 
-	/**
+  /**
 	 * It updates the user's status in the database and then triggers a Pusher event to update the user's
 	 * status in the frontend
 	 * @param {number} id - The id of the user whose status is being updated.
 	 * @param {"online" | "offline"} status - "online" | "offline"
 	 * @returns The response from the Pusher API.
 	 */
-	async updateStatus(
-		id: number,
-		status: "online" | "offline"
-	): Promise<Pusher.Response> {
-		const oldUser = await this.findOneByIdNoRelations(id);
-		oldUser.userStatus = status;
-		await this.usersRepository.update(id, oldUser);
+  async updateStatus (
+    id: number,
+    status: 'online' | 'offline'
+  ): Promise<Pusher.Response> {
+    const oldUser = await this.findOneByIdNoRelations(id)
+    oldUser.userStatus = status
+    await this.usersRepository.update(id, oldUser)
 
-		return await this.pusherService.trigger(
+    return await this.pusherService.trigger(
 			`userStatus-${id}`,
-			"statusUpdate",
+			'statusUpdate',
 			{ id, status }
-		);
-	}
+    )
+  }
 
-	/**
+  /**
 	 * This function returns a promise that resolves to either the string "online" or the string "offline"
 	 * depending on the user's status.
 	 * @param {number} id - number - the id of the user
 	 * @returns The userStatus property of the user object.
 	 */
-	async getStatus(id: number): Promise<"online" | "offline"> {
-		const user = await this.findOneByIdNoRelations(id);
-		return user.userStatus;
-	}
+  async getStatus (id: number): Promise<'online' | 'offline'> {
+    const user = await this.findOneByIdNoRelations(id)
+    return user.userStatus
+  }
 
-	/**
+  /**
 	 * Find a user by id, then soft remove it.
 	 * @param {number} id - number - the id of the user to be deleted
 	 */
-	async removeSoft(id: number): Promise<void> {
-		const user = await this.findOneById(id);
-		await this.usersRepository.softRemove(user);
-	}
+  async removeSoft (id: number): Promise<void> {
+    const user = await this.findOneById(id)
+    await this.usersRepository.softRemove(user)
+  }
 
-	/**
+  /**
 	 * It returns a promise that resolves to an object with two properties, users and jobs, where users is
 	 * an array of users and jobs is an array of jobs.
 	 * The function takes two arguments, user
@@ -199,108 +199,108 @@ export class UsersService {
 	 * @param {string} query - string - The search query
 	 * @returns An object with two properties: users and jobs.
 	 */
-	public async search(
-		user: User | null,
-		query: string
-	): Promise<Users.SearchResponse> {
-		return {
-			users: await this.usersRepository.find({
-				where: [
-					{ firstName: Like(`%${query}%`) },
-					{ lastName: Like(`%${query}%`) },
-					{ email: Like(`%${query}%`) },
-				],
-				take: 10,
-			}),
-			jobs: await this.jobsRepository.find({
-				where: [
-					{ jobTitle: Like(`%${query}%`) },
-					{ companyName: Like(`%${query}%`) },
-					{ location: Like(`%${query}%`) },
-				],
-				take: 10,
-			}),
-		};
-	}
+  public async search (
+    user: User | null,
+    query: string
+  ): Promise<Users.SearchResponse> {
+    return {
+      users: await this.usersRepository.find({
+        where: [
+          { firstName: Like(`%${query}%`) },
+          { lastName: Like(`%${query}%`) },
+          { email: Like(`%${query}%`) }
+        ],
+        take: 10
+      }),
+      jobs: await this.jobsRepository.find({
+        where: [
+          { jobTitle: Like(`%${query}%`) },
+          { companyName: Like(`%${query}%`) },
+          { location: Like(`%${query}%`) }
+        ],
+        take: 10
+      })
+    }
+  }
 
-	/**
+  /**
 	 * It takes a user and files, and if the files have a cv or cover letter, it converts the buffer to
 	 * base64 and saves it to the user.
 	 * @param {User} user - User - the user object that is being updated
 	 * @param files - { cv?: Express.Multer.File; coverLetter?: Express.Multer.File }
 	 */
-	async addDocuments(
-		user: User,
-		files: { cv?: Express.Multer.File; coverLetter?: Express.Multer.File }
-	): Promise<void> {
-		if (files?.cv != null) {
-			const buff = files.cv[0].buffer;
-			const base64data = buff.toString("base64");
-			user.cv = base64data;
-		}
+  async addDocuments (
+    user: User,
+    files: { cv?: Express.Multer.File, coverLetter?: Express.Multer.File }
+  ): Promise<void> {
+    if (files?.cv != null) {
+      const buff = files.cv[0].buffer
+      const base64data = buff.toString('base64')
+      user.cv = base64data
+    }
 
-		if (files?.coverLetter != null) {
-			const buff = files.coverLetter[0].buffer;
-			const base64data = buff.toString("base64");
-			user.coverLetter = base64data;
-		}
+    if (files?.coverLetter != null) {
+      const buff = files.coverLetter[0].buffer
+      const base64data = buff.toString('base64')
+      user.coverLetter = base64data
+    }
 
-		await this.usersRepository.save(user);
-	}
+    await this.usersRepository.save(user)
+  }
 
-	/**
+  /**
 	 * It removes documents from a user's profile.
 	 * @param {User} user - User - this is the user that is currently logged in
 	 * @param data - Users.DeleteDocumentsRequest
 	 */
-	async removeDocuments(
-		user: User,
-		data: Users.DeleteDocumentsRequest
-	): Promise<void> {
-		if (data.cv) {
-			user.cv = null;
-		}
+  async removeDocuments (
+    user: User,
+    data: Users.DeleteDocumentsRequest
+  ): Promise<void> {
+    if (data.cv) {
+      user.cv = null
+    }
 
-		if (data.coverLetter) {
-			user.coverLetter = null;
-		}
+    if (data.coverLetter) {
+      user.coverLetter = null
+    }
 
-		await this.usersRepository.save(user);
-	}
+    await this.usersRepository.save(user)
+  }
 
-	/**
+  /**
 	 * It finds a user by id, sets the profilePic property to null, and saves the user.
 	 * @param {number} userId - number - The id of the user whose profile picture you want to remove.
 	 */
-	async removeProfilePic(userId: number): Promise<void> {
-		const user = await this.usersRepository.findOneOrFail({
-			where: {
-				id: userId,
-			},
-		});
-		user.profilePic = null;
-		await this.usersRepository.save(user);
-	}
+  async removeProfilePic (userId: number): Promise<void> {
+    const user = await this.usersRepository.findOneOrFail({
+      where: {
+        id: userId
+      }
+    })
+    user.profilePic = null
+    await this.usersRepository.save(user)
+  }
 
-	/**
+  /**
 	 * It finds a user by id, sets the coverPic property to null, and saves the user.
 	 * @param {number} userId - number - the id of the user whose cover pic you want to remove
 	 */
-	async removeCoverPic(userId: number): Promise<void> {
-		const user = await this.usersRepository.findOneOrFail({
-			where: {
-				id: userId,
-			},
-		});
-		user.coverPic = null;
-		await this.usersRepository.save(user);
-	}
+  async removeCoverPic (userId: number): Promise<void> {
+    const user = await this.usersRepository.findOneOrFail({
+      where: {
+        id: userId
+      }
+    })
+    user.coverPic = null
+    await this.usersRepository.save(user)
+  }
 
-	public async report(user: User, data: Users.ReportRequest) {
-		const report = new Report();
-		report.reported_by_id = user.id;
-		report.entity_id = data.entity_id;
-		report.type = data.type;
-		report.save();
-	}
+  public async report (user: User, data: Users.ReportRequest): Promise<void> {
+    const report = new Report()
+    report.reported_by_id = user.id
+    report.entity_id = data.entity_id
+    report.type = data.type
+    await report.save()
+  }
 }
