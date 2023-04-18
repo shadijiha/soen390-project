cac8e2fe328fc404063642f3d97026053cc24f27
Marc-Eid <marc.mn.eid@gmail.com>
2023-04-17 22:16:50 -0400

    added a notification for user when a job is posted that matches their skills #274

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -5,10 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Job } from '../models/job.entity'
 import { Skill } from '../models/skill.entity'
 import { User } from '../models/user.entity'
+import { NotificationsService } from '../users/notifications/notifications.service'
+import { Notifications } from '../models/notifications.entity'
+import { PusherService } from '../util/pusher/pusher.service'
 
 @Module({
-  providers: [JobsService],
+  providers: [JobsService, NotificationsService, PusherService],
   controllers: [JobsController],
-  imports: [TypeOrmModule.forFeature([Job, User, Skill])]
+  imports: [TypeOrmModule.forFeature([Job, User, Skill, Notifications])]
 })
 export class JobsModule {}
