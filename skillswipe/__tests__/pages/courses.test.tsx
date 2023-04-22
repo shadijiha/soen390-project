d9c708713168782d0510ef7b904435d7a108c2da
Aleksandr Vinokhodov <vinokhodovalex@gmail.com>
2023-04-22 14:14:44 -0400

    front: fix courses test

---
+++ skillswipe/__tests__/pages/courses.test.tsx
@@ -0,0 +1,13 @@
+import Courses from '@/components/Profile/Courses'
+import { render, screen } from '../test-utils'
+
+jest.mock('next/router', () => require('next-router-mock'))
+
+describe('courses', () => {
+  const renderCourses = () => render(<Courses />)
+
+  it('should render courses page without crashing', () => {
+    renderCourses()
+    expect(screen.queryByTestId('courses'))
+  })
+})
